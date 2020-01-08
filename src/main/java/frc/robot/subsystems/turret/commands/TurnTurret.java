709f2072ea7fc8c65c6583a728121cb89ccf2a80
Adam <adam.vichik@gmail.com>
2020-01-08 18:06:18 +0200

    Implement TurnTurret methods

--- src/main/java/frc/robot/subsystems/turret/commands/TurnTurret.java
+++ src/main/java/frc/robot/subsystems/turret/commands/TurnTurret.java
@@ -1,4 +1,45 @@
 package frc.robot.subsystems.turret.commands;
 
-public class TurnTurret {
+
+import edu.wpi.first.wpilibj2.command.*;
+
+import static frc.robot.Constants.Turret.*;
+import static frc.robot.Robot.turret;
+
+/**
+ *
+ */
+public class TurnTurret extends CommandBase {
+
+    private double angle;
+
+    public TurnTurret(double angle) {
+        addRequirements(turret);
+        this.angle = angle;
+    }
+
+
+    // Called just before this Command runs the first time
+    @Override
+    public void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+        turret.moveTurret(angle);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        turret.stop();
+    }
+
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return Math.abs(turret.getAngle() - angle) <= ANGLE_THRESHOLD;
+    }
+
 }
