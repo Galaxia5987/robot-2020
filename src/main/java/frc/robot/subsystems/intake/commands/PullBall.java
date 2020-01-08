ec5580e802b1c0ebf0142d1dcbf60d1e545949d7
Barel Shlidor <barel@shlidor.org>
2020-01-08 19:45:30 +0200

    Create PullBall constructors

--- src/main/java/frc/robot/subsystems/intake/commands/PullBall.java
+++ src/main/java/frc/robot/subsystems/intake/commands/PullBall.java
@@ -1,12 +1,28 @@
 package frc.robot.subsystems.intake.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
 
 public class PullBall extends CommandBase {
+    private Timer timer = new Timer();
+    private double speed;
+    private double timeout;
+
+    public PullBall(double speed, double timeout) {
+        addRequirements(Robot.intake);
+        this.speed = speed;
+        this.timeout = timeout;
+    }
+
+    public PullBall(double speed) {
+        this(speed, 0); 
+    }
+
 
     @Override
     public void initialize() {
-        
+
     }
 
     @Override
