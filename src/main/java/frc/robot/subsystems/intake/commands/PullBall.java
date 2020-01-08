b2524ec8a790e74e6d8163d2371f499deb8c5553
Barel Shlidor <barel@shlidor.org>
2020-01-08 19:48:45 +0200

    implement PullBall methods

--- src/main/java/frc/robot/subsystems/intake/commands/PullBall.java
+++ src/main/java/frc/robot/subsystems/intake/commands/PullBall.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.intake.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Robot;
+import frc.robot.subsystems.intake.Intake;
+
+import static frc.robot.Robot.intake;
 
 public class PullBall extends CommandBase {
     private Timer timer = new Timer();
@@ -10,33 +12,36 @@ public class PullBall extends CommandBase {
     private double timeout;
 
     public PullBall(double speed, double timeout) {
-        addRequirements(Robot.intake);
+        addRequirements(intake);
         this.speed = speed;
         this.timeout = timeout;
     }
 
     public PullBall(double speed) {
-        this(speed, 0); 
+        this(speed, 0);
     }
-
 
     @Override
     public void initialize() {
-
+        timer.reset();
+        timer.start();
+        intake.setPosition(Intake.Direction.DOWN);
+        intake.applyPowerOnWheels(speed);
     }
 
     @Override
     public void execute() {
-
+        intake.applyPowerOnWheels(speed);
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        timer.stop();
+        intake.applyPowerOnWheels(0);
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return timer.get() >= timeout;
     }
 }
