3d1fd505ed6d9effd835509743ec3c3d964af66e
Barel Shlidor <barel@shlidor.org>
2020-01-08 20:18:53 +0200

    Change Solenoid to DoubleSolenoid

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,6 +3,8 @@ package frc.robot;
 public final class Ports {
     public static final class Intake {
         public static final int MASTER = 0;
-        public static final int SOLENOID = 0;
+        //TODO: Check, may need to switch between the channels
+        public static final int SOLENOID_FORWARD_CHANNEL = 0;
+        public static final int SOLENOID_BACKWARD_CHANNEL = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.Intake.*;
@@ -10,7 +10,7 @@ import static frc.robot.Ports.Intake.*;
 
 public class Intake extends SubsystemBase {
     private VictorSPX masterMotor = new VictorSPX(MASTER);
-    private Solenoid solenoid = new Solenoid(SOLENOID);
+    private DoubleSolenoid solenoid = new DoubleSolenoid(SOLENOID_FORWARD_CHANNEL, SOLENOID_BACKWARD_CHANNEL);
 
     public Intake() {
         masterMotor.setInverted(MASTER_INVERTED);
