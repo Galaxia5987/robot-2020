7274cb8c8e4143dbc29f708d1de5a931f2d03d4a
Orel David Aboukasis <orel.david1502@gmail.com>
2020-01-08 21:37:47 +0200

    Create a command for the climbing

--- src/main/java/frc/robot/subsystems/climb/commands/RiseToHeightCommand.java
+++ src/main/java/frc/robot/subsystems/climb/commands/RiseToHeightCommand.java
@@ -5,25 +5,28 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.subsystems.climb.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.Constants;
+import frc.robot.subsystems.climb.Climb;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
  * An example command that uses an example subsystem.
  */
-public class ExampleCommand extends CommandBase {
+public class RiseToHeightCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
-
+  private final Climb m_subsystem;
+  private double targetHeight;
+  private double currentHeight;
   /**
-   * Creates a new ExampleCommand.
+   * Creates a new RiseToHeightCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
+  public RiseToHeightCommand(Climb subsystem, double targetHeight) {
     m_subsystem = subsystem;
+    this.targetHeight = targetHeight;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
@@ -31,21 +34,29 @@ public class ExampleCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    m_subsystem.releaseStopper();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    currentHeight = (m_subsystem.getLeftHeight()+m_subsystem.getRightHeight())/2;
+    if (Math.abs(targetHeight-currentHeight) < Constants.Climb.CLIMB_TOLERANCE){
+      m_subsystem.engageStopper();
+    }
+    m_subsystem.setLeftHeight(targetHeight);
+    m_subsystem.setRightHeight(targetHeight);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_subsystem.engageStopper();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return Math.abs(currentHeight) < Constants.Climb.CLIMB_TOLERANCE;
   }
 }
