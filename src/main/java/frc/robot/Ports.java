42b203d946e9a83e019e8913263c6be716789c6e
Adam <adam.vichik@gmail.com>
2020-01-08 19:58:13 +0200

    Create Turret Ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,4 +1,10 @@
 package frc.robot;
 
 public class Ports {
+
+    public static class Turret {
+        public static final int MASTER = 0;
+        public static final boolean IS_MASTER_INVERTED  = false;
+        public static final boolean IS_ENCODER_INVERTED  = false;
+    }
 }
