92f534f5cb05b00b248b8c78aac68bc3967e2ea8
Barel Shlidor <barel@shlidor.org>
2020-01-08 18:01:21 +0200

    Create Solenoid instance in the subsystem

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,5 +3,6 @@ package frc.robot;
 public final class Ports {
     public static final class Intake {
         public static final int MASTER = 0;
+        public static final int SOLENOID = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,13 +1,15 @@
 package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Constants.Intake.*;
-import static frc.robot.Ports.Intake.MASTER;
+import static frc.robot.Ports.Intake.*;
 
 public class Intake extends SubsystemBase {
     private VictorSPX master = new VictorSPX(MASTER);
+    private Solenoid solenoid = new Solenoid(SOLENOID);
 
     public Intake() {
         master.setInverted(MASTER_INVERTED);
