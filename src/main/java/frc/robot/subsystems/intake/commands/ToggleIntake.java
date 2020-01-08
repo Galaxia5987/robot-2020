beedcfc1b588807743a763e56fbab06b8cae6b0c
Barel Shlidor <barel@shlidor.org>
2020-01-08 19:57:11 +0200

    Create ToggleIntake command

---
+++ src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class ToggleIntake extends CommandBase {
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
