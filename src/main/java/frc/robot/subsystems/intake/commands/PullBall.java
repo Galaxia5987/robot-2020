0acdd1348d655b0234ee0755e314bc1d2ef0779d
Barel Shlidor <barel@shlidor.org>
2020-01-08 18:14:49 +0200

    Create PullBall command

---
+++ src/main/java/frc/robot/subsystems/intake/commands/PullBall.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PullBall extends CommandBase {
+
+    @Override
+    public void initialize() {
+        
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
