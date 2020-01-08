e5b7536c203a8bab9a502bf0084774f97b0e3f04
Barel Shlidor <barel@shlidor.org>
2020-01-08 19:53:38 +0200

    Implement ShootBall command

--- src/main/java/frc/robot/subsystems/intake/commands/ShootBall.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ShootBall.java
@@ -1,26 +1,47 @@
 package frc.robot.subsystems.intake.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake;
+
+import static frc.robot.Robot.intake;
 
 public class ShootBall extends CommandBase {
-    
+    private Timer timer = new Timer();
+    private double speed;
+    private double timeout;
+
+    public ShootBall(double speed, double timeout) {
+        addRequirements(intake);
+        this.speed = -speed;
+        this.timeout = timeout;
+    }
+
+    public ShootBall(double speed) {
+        this(speed, 0);
+    }
+
     @Override
     public void initialize() {
-
+        timer.reset();
+        timer.start();
+        intake.setPosition(Intake.Direction.DOWN);
+        intake.applyPowerOnWheels(speed);
     }
 
     @Override
     public void execute() {
-
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-
+        intake.applyPowerOnWheels(speed);
     }
 
     @Override
     public boolean isFinished() {
-        return false;
+        return timer.get() >= timeout;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        timer.stop();
+        intake.applyPowerOnWheels(0);
     }
 }
