6e9cfef4c9abd733b750b67145e1042b895676c1
Orel David Aboukasis <orel.david1502@gmail.com>
2020-01-08 20:36:08 +0200

    Add the ports of the climb subsystem

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,4 +1,11 @@
 package frc.robot;
 
 public class Ports {
+    public class climb {
+        public static final int leftClimbMaster = 0;
+        public static final int rightClimbMaster = 0;
+        public static final int solenoidForward = 0;
+        public static final int solenoidReverse = 0;
+        public static final int moduleNumber = 1;
+    }
 }
