2237ae3bdb875d73bb2572b8366a0b96a39adbe6
Barel Shlidor <barel@shlidor.org>
2020-01-08 17:56:07 +0200

    Migrate extending to SubsystemBase

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,10 +1,10 @@
 package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static frc.robot.Ports.Intake.MASTER;
 
-public class Intake implements Subsystem {
+public class Intake extends SubsystemBase {
     private VictorSPX master = new VictorSPX(MASTER);
 }
