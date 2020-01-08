ce6cc0994c77489bda8de822d0c660eaace073c8
Barel Shlidor <barel@shlidor.org>
2020-01-08 18:11:21 +0200

    Add applyPower function

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -8,11 +9,11 @@ import static frc.robot.Constants.Intake.*;
 import static frc.robot.Ports.Intake.*;
 
 public class Intake extends SubsystemBase {
-    private VictorSPX master = new VictorSPX(MASTER);
+    private VictorSPX masterMotor = new VictorSPX(MASTER);
     private Solenoid solenoid = new Solenoid(SOLENOID);
 
     public Intake() {
-        master.setInverted(MASTER_INVERTED);
+        masterMotor.setInverted(MASTER_INVERTED);
     }
 
     public void setPosition(Direction direction) {
@@ -34,6 +35,10 @@ public class Intake extends SubsystemBase {
         if (solenoid.get())
             return Direction.UP;
         return Direction.DOWN;
+    }
+
+    public void applyPowerOnWheels(double speed) {
+        masterMotor.set(ControlMode.PercentOutput, speed);
     }
 
     public enum Direction {
