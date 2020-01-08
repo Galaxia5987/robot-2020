b907a1f44cb2617d0d3bea301bf81dc4dec4ba56
Orel David Aboukasis <orel.david1502@gmail.com>
2020-01-08 20:39:16 +0200

    Configure the settings of the climb subsytem

---
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -0,0 +1,56 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.climb;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Ports;
+
+public class Climb extends SubsystemBase {
+
+    private TalonSRX leftClimbMaster = new TalonSRX(Ports.climb.leftClimbMaster);
+    private TalonSRX rightClimbMaster = new TalonSRX(Ports.climb.rightClimbMaster);
+    private DoubleSolenoid stopper = new DoubleSolenoid(Ports.climb.moduleNumber, Ports.climb.solenoidForward, Ports.climb.solenoidReverse);
+
+    /**
+     * Creates a new climb Subsystem.
+     */
+    public Climb() {
+      leftClimbMaster.setInverted(Constants.Climb.LEFT_IS_REVERSED);
+      rightClimbMaster.setInverted(Constants.Climb.RIGHT_IS_REVERSED);
+
+      leftClimbMaster.configMotionCruiseVelocity(Constants.Climb.MOTION_MAGIC_VELOCITY);
+      rightClimbMaster.configMotionCruiseVelocity(Constants.Climb.MOTION_MAGIC_VELOCITY);
+
+      leftClimbMaster.configMotionAcceleration(Constants.Climb.MOTION_MAGIC_ACCELERATION);
+      rightClimbMaster.configMotionAcceleration(Constants.Climb.MOTION_MAGIC_ACCELERATION);
+
+      leftClimbMaster.config_kP(0, Constants.Climb.CLIMB_PIDF[0]);
+      rightClimbMaster.config_kP(0, Constants.Climb.CLIMB_PIDF[0]);
+      leftClimbMaster.config_kI(0, Constants.Climb.CLIMB_PIDF[1]);
+      rightClimbMaster.config_kI(0, Constants.Climb.CLIMB_PIDF[1]);
+      leftClimbMaster.config_kD(0, Constants.Climb.CLIMB_PIDF[2]);
+      rightClimbMaster.config_kD(0, Constants.Climb.CLIMB_PIDF[2]);
+      leftClimbMaster.config_kF(0, Constants.Climb.CLIMB_PIDF[3]);
+      rightClimbMaster.config_kF(0, Constants.Climb.CLIMB_PIDF[3]);
+
+      leftClimbMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+      rightClimbMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+
+      leftClimbMaster.setSensorPhase(Constants.Climb.LEFT_SENSOR_PHASE);
+      rightClimbMaster.setSensorPhase(Constants.Climb.RIGHT_SENSOR_PHASE);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
