56163bca888577b9b1a4b25a4c7c6edddfdc2432
Barel Shlidor <barel@shlidor.org>
2020-01-08 20:05:59 +0200

    Implement ToggleIntake command

--- src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
+++ src/main/java/frc/robot/subsystems/intake/commands/ToggleIntake.java
@@ -1,26 +1,45 @@
 package frc.robot.subsystems.intake.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.intake.Intake.Direction;
+
+import static frc.robot.Robot.intake;
 
 public class ToggleIntake extends CommandBase {
+    private Direction direction;
+
+    public ToggleIntake(Direction direction) {
+        addRequirements(intake);
+        this.direction = direction;
+    }
+
+    public ToggleIntake() {
+        addRequirements(intake);
+        if (intake.getPosition() == Direction.UP) {
+            direction = Direction.DOWN;
+            return;
+        }
+        direction = Direction.UP;
+    }
 
     @Override
     public void initialize() {
-
+        intake.setPosition(direction);
     }
 
     @Override
     public void execute() {
-
+        intake.changePositionAutomatically();
     }
+
+
 
     @Override
     public boolean isFinished() {
-        return false;
+        return intake.getPosition() == direction;
     }
 
     @Override
     public void end(boolean interrupted) {
-
     }
 }
