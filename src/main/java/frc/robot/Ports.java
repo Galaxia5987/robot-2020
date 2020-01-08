693b99d3d8a8a5597a2c4ebe2a5032987dff1e0d
Barel Shlidor <barel@shlidor.org>
2020-01-08 17:53:08 +0200

    Create VictorSRX instance

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,6 +2,6 @@ package frc.robot;
 
 public final class Ports {
     public static final class Intake {
-        
+        public static final int MASTER = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.intake;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+import static frc.robot.Ports.Intake.MASTER;
+
 public class Intake implements Subsystem {
+    private VictorSPX master = new VictorSPX(MASTER);
 }
