5ab4cfe11d5d11109f9a4b326d54a4ccf1e77587
Repulser <thetechproject2014@gmail.com>
2020-01-09 17:17:55 +0200

    Touchup naming in UnitModel

--- src/main/java/frc/robot/subsystems/UnitModel.java
+++ src/main/java/frc/robot/subsystems/UnitModel.java
@@ -7,11 +7,11 @@ public class UnitModel {
         this.ticksPerUnit = ticksPerUnit;
     }
 
-    public double convertToUnits(double ticks) {
+    public double toUnits(double ticks) {
         return ticks / ticksPerUnit;
     }
 
-    public int convertToTicks(double units) {
+    public int toTicks(double units) {
         return (int) (ticksPerUnit * units);
     }
 
