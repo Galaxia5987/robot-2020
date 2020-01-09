8fea22c8f7b5f33c8d825ed9d205d72a1ea8963c
Orel David Aboukasis <orel.david1502@gmail.com>
2020-01-09 20:48:08 +0200

    Add to the periodic to check if it reaches the limit reset the encoder

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -133,6 +133,17 @@ public class Climb extends SubsystemBase {
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+
+        if (isLeftOnLimit()) {
+            leftReset();
+        }
+
+        if (isRightOnLimit()) {
+            rightReset();
+        }
+
+        if (isRightOnLimit() || isLeftOnLimit()) {
+            engageStopper();
+        }
     }
 }
