3dcb1197052197af4eb30227408a5669c3f33d7b
oreld <orel.david1502@gmail.com>
2020-01-09 19:15:29 +0200

    Reformat command

--- src/main/java/frc/robot/subsystems/climb/commands/RiseToHeightCommand.java
+++ src/main/java/frc/robot/subsystems/climb/commands/RiseToHeightCommand.java
@@ -15,48 +15,49 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
  * An example command that uses an example subsystem.
  */
 public class RiseToHeightCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Climb m_subsystem;
-  private double targetHeight;
-  private double currentHeight;
-  /**
-   * Creates a new RiseToHeightCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public RiseToHeightCommand(Climb subsystem, double targetHeight) {
-    m_subsystem = subsystem;
-    this.targetHeight = targetHeight;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
-  }
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final Climb m_subsystem;
+    private double targetHeight;
+    private double currentHeight;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    m_subsystem.releaseStopper();
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    currentHeight = (m_subsystem.getLeftHeight()+m_subsystem.getRightHeight())/2;
-    if (Math.abs(targetHeight-currentHeight) < Constants.Climb.CLIMB_TOLERANCE){
-      m_subsystem.engageStopper();
+    /**
+     * Creates a new RiseToHeightCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public RiseToHeightCommand(Climb subsystem, double targetHeight) {
+        m_subsystem = subsystem;
+        this.targetHeight = targetHeight;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
     }
-    m_subsystem.setLeftHeight(targetHeight);
-    m_subsystem.setRightHeight(targetHeight);
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    m_subsystem.engageStopper();
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        m_subsystem.releaseStopper();
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return Math.abs(currentHeight) < Constants.Climb.CLIMB_TOLERANCE;
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        currentHeight = (m_subsystem.getLeftHeight() + m_subsystem.getRightHeight()) / 2;
+        if (Math.abs(targetHeight - currentHeight) < Constants.Climb.CLIMB_TOLERANCE) {
+            m_subsystem.engageStopper();
+        }
+        m_subsystem.setLeftHeight(targetHeight);
+        m_subsystem.setRightHeight(targetHeight);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_subsystem.engageStopper();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return Math.abs(currentHeight) < Constants.Climb.CLIMB_TOLERANCE;
+    }
 }
