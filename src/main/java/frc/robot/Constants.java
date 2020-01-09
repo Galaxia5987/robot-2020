41ee2039996f2525269a2afbd2ef657259c30570
Adam <adam.vichik@gmail.com>
2020-01-09 07:57:12 +0200

    Add ports class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,22 +19,22 @@ public final class Constants {
 
     public static class Shooter {
         public static final int TALON_PID_SLOT = 0;
-        public static final double KP = .57;
-        public static final double KI = 0;
-        public static final double KD = 0.31;
-        public static final double KF = 0;
-        public static final int TIMEOUT_MS = 20;
-        public static final boolean IS_MASTER_INVERTED = false;
-        public static final boolean IS_SLAVE_INVERTED = true;
+        public static final double KP = 0.085; // 0.13
+        public static final double KI = 0.0;
+        public static final double KD = 0.00;
+        public static final double KF = 0.003;
+        public static final int TALON_TIMEOUT = 10;
         public static final int MAX_CURRENT = 35; //[A]
-        public static final double TICKS_PER_METER = 36 / (2 * 0.05 * Math.PI);
+        public static final double RADIUS = 0.05; // [m]
         public static final double TICKS_PER_ROTATION = 36;
         public static final double HEIGHT = 0; // [m]
         public static final double ANGLE = 45; // [deg]
-        public static final double RADIUS = 0.05; // [m]
         public static final double g = 9.80665; // [m/sec^2]
         public static final double TARGET_DISTANCE = 2; // [m]
         public static final double SHOOTING_TIME = 3.5;
+        public static final double TARGET_RPM = 500;
+        public static final double PERCENT_THRESHOLD = 10;
+        public static final double RAMP_RATE = 2;
     }
 
 }
