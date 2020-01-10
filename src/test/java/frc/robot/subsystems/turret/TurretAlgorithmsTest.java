38da54384d5f76813fdbc84344df78e255f3fd28
Adam <adam.vichik@gmail.com>
2020-01-10 15:18:25 +0200

    Create tests for setTurretAngle() and constrain()

---
+++ src/test/java/frc/robot/subsystems/turret/TurretAlgorithmsTest.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.turret;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TurretAlgorithmsTest {
+
+    private TurretAlgorithms algorithmsTest = new TurretAlgorithms();
+
+    @Test
+    public void setTurretAngle() {
+        double targetAngle = 280;
+        double currentPosition = -40;
+        double minPos = -359;
+        double maxPos = 359;
+
+        Assert.assertEquals(algorithmsTest.setTurretAngle(targetAngle,currentPosition, minPos, maxPos), -80, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(-40,30, minPos, maxPos), -40, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(300,-40, minPos, maxPos), -60, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(20,320, minPos, maxPos), 20, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(320,20, minPos, maxPos), -40, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(173,-247, minPos, maxPos), -187, 0.1);
+    }
+
+    @Test
+    public void constrain() {
+        double targetAngle = 700;
+        double minPos = -359;
+        double maxPos = 359;
+
+        Assert.assertEquals(algorithmsTest.constrain(minPos, targetAngle, maxPos), 359, 2);
+    }
+}
