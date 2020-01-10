6bf7c341d38858e429a1fb28f05527731fb754f7
lior <lior5858@gmail.com>
2020-01-10 14:20:50 +0200

    Configure the drivetrain motors

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,9 +7,15 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXPIDSetConfiguration;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Ports;
+import frc.robot.UtilityFunctions;
+import frc.robot.utilities.FalconConfiguration;
 
 public class Drivetrain extends SubsystemBase {
 
@@ -18,10 +24,20 @@ public class Drivetrain extends SubsystemBase {
   private final TalonFX rightSlave = new TalonFX(Ports.Drivetrain.RIGHT_SLAVE);
   private final TalonFX leftMaster = new TalonFX(Ports.Drivetrain.LEFT_MASTER);
   private final TalonFX leftSlave = new TalonFX(Ports.Drivetrain.LEFT_SLAVE);
+  private FalconConfiguration configurations = new FalconConfiguration();
+  private double[] pidSet = {Constants.Drivetrain.KP, Constants.Drivetrain.KI, Constants.Drivetrain.KD, Constants.Drivetrain.KF};
+
   /**
    * Creates a new ExampleSubsystem.
    */
   public Drivetrain() {
+    rightSlave.follow(rightMaster);
+    leftSlave.follow(leftMaster);
+    configurations.setNeutralMode(NeutralMode.Coast);
+    configurations.setFeedbackDevice(FeedbackDevice.QuadEncoder);
+    configurations.setEnableVoltageCompensation(true);
+
+    UtilityFunctions.configAllFalcons(configurations, rightMaster, rightSlave, leftMaster, leftSlave);
 
   }
 
