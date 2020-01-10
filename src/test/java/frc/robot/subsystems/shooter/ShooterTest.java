aafc304ee3b58182a2e70fc385bb03169071ef64
Adam <adam.vichik@gmail.com>
2020-01-10 11:41:11 +0200

    Create ShooterTest

---
+++ src/test/java/frc/robot/subsystems/shooter/ShooterTest.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.shooter;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ShooterTest {
+    private Shooter shooter;
+
+    @Before
+    public void createShooterInstance() {
+        shooter = new Shooter();
+    }
+
+
+    @Test
+    public void ticksToRPM() {
+        int ticks = 12;
+        double desired_RPM = 1.2;
+        double RPM_res = shooter.ticksToRPM(12);
+
+        Assert.assertEquals (desired_RPM, RPM_res,1e-10);
+    }
+
+    @Test
+    public void RPMToTicks() {
+    }
+}
