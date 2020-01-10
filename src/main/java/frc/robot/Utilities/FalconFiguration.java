980ee7f93d3085e1e12396bcf369f51425279474
lior <lior5858@gmail.com>
2020-01-10 12:16:43 +0200

    Create a falcon configuration class and function

---
+++ src/main/java/frc/robot/Utilities/FalconFiguration.java
@@ -0,0 +1,100 @@
+package frc.robot.Utilities;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
+import com.ctre.phoenix.motorcontrol.LimitSwitchSource;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonFXPIDSetConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXPIDSetConfiguration;
+
+public class FalconFiguration {
+
+    private NeutralMode neutralMode;
+    private FeedbackDevice feedbackDevice;
+    private boolean enableVoltageCompensation;
+    public TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
+
+
+    public FalconFiguration() {
+
+        neutralMode = NeutralMode.Coast;
+        feedbackDevice = FeedbackDevice.CTRE_MagEncoder_Absolute;
+        enableVoltageCompensation = false;
+
+
+    }
+
+    public NeutralMode getNeutralMode() {
+        return this.neutralMode;
+    }
+
+    public FeedbackDevice getFeedbackDevice() {
+        return feedbackDevice;
+    }
+
+    public boolean isEnableVoltageCompensation() {
+        return enableVoltageCompensation;
+    }
+
+    public void setNeutralMode(NeutralMode neutralMode) {
+        this.neutralMode = neutralMode;
+    }
+
+    public void setFeedbackDevice(FeedbackDevice feedbackDevice) {
+        this.feedbackDevice = feedbackDevice;
+    }
+
+    public void setEnableVoltageCompensation(boolean enableVoltageCompensation) {
+        this.enableVoltageCompensation = enableVoltageCompensation;
+    }
+
+    public void setPrimaryPID(TalonFXPIDSetConfiguration primaryPID) {
+        this.motorConfigs.primaryPID = primaryPID;
+    }
+
+    public void setAuxiliaryPID(TalonFXPIDSetConfiguration auxiliaryPID) {
+        this.motorConfigs.auxiliaryPID = auxiliaryPID;
+    }
+
+    public void setForwardLimitSwitchSource(LimitSwitchSource forwardLimitSwitchSource) {
+        this.motorConfigs.forwardLimitSwitchSource = forwardLimitSwitchSource;
+    }
+
+    public void setReverseLimitSwitchSource(LimitSwitchSource reverseLimitSwitchSource) {
+        this.motorConfigs.reverseLimitSwitchSource = reverseLimitSwitchSource;
+    }
+
+    public void setForwardLimitSwitchDeviceID(int forwardLimitSwitchDeviceID) {
+        this.motorConfigs.forwardLimitSwitchDeviceID = forwardLimitSwitchDeviceID;
+    }
+
+    public void setReverseLimitSwitchDeviceID(int reverseLimitSwitchDeviceID) {
+        this.motorConfigs.reverseLimitSwitchDeviceID = reverseLimitSwitchDeviceID;
+    }
+
+    public void setForwardLimitSwitchNormal(LimitSwitchNormal forwardLimitSwitchNormal) {
+        this.motorConfigs.forwardLimitSwitchNormal = forwardLimitSwitchNormal;
+    }
+
+    public void setReverseLimitSwitchNormal(LimitSwitchNormal reverseLimitSwitchNormal) {
+        this.motorConfigs.reverseLimitSwitchNormal = reverseLimitSwitchNormal;
+    }
+
+    public void setSum0Term(FeedbackDevice sum0Term) {
+        this.motorConfigs.sum0Term = sum0Term;
+    }
+
+    public void setSum1Term(FeedbackDevice sum1Term) {
+        this.motorConfigs.sum1Term = sum1Term;
+    }
+
+    public void setDiff0Term(FeedbackDevice diff0Term) {
+        this.motorConfigs.diff0Term = diff0Term;
+    }
+
+    public void setDiff1Term(FeedbackDevice diff1Term) {
+        this.motorConfigs.diff1Term = diff1Term;
+    }
+}
---
+++ src/main/java/frc/robot/UtilityFunctions.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import frc.robot.Utilities.FalconFiguration;
+
+public class UtilityFunctions {
+    public static void configAllFalcons(FalconFiguration configs, TalonFX... falcons) {
+        for (TalonFX falcon : falcons) {
+            falcon.configAllSettings(configs.motorConfigs);
+            falcon.setNeutralMode(NeutralMode.Brake);
+            falcon.configSelectedFeedbackSensor(configs.getFeedbackDevice());
+            falcon.enableVoltageCompensation(configs.isEnableVoltageCompensation());
+
+        }
+    }
+}
