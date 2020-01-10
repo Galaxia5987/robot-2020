e6940c7a35a7bba3aafb4a7b78e941bc9f1e3960
Barel Shlidor <barel@shlidor.org>
2020-01-10 09:43:14 +0200

    Create Serializer Subsystem

---
+++ src/main/java/frc/robot/subsystems/serializer/Serializer.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.serializer;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Serializer extends SubsystemBase {
+    public Serializer() {
+
+    }
+}
