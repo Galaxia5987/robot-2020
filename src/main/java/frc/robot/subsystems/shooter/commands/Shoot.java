9a51df79d092aa5659d557d5ac0b8273d1adc770
Adam <adam.vichik@gmail.com>
2020-01-10 08:32:07 +0200

    Create Shoot Command

--- src/main/java/frc/robot/subsystems/shooter/commands/Shoot.java
+++ src/main/java/frc/robot/subsystems/shooter/commands/Shoot.java
@@ -1,4 +1,75 @@
 package frc.robot.subsystems.shooter.commands;
 
-public class Shoot {
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import static frc.robot.Robot.shooter;
+import static frc.robot.Constants.Shooter.*;
+
+public class Shoot extends CommandBase {
+    private double distance;
+    private double timeout;
+    private Timer timer = new Timer();
+    private static NetworkTable shooterTable = new NetworkTableInstance().getDefault().getTable("shooter");
+    private NetworkTableEntry velocityEntry = shooterTable.getEntry("velocity");
+
+    public Shoot(double distance, double timeout) {
+        addRequirements(shooter);
+        this.distance = distance;
+        this.timeout = timeout;
+    }
+
+    public Shoot(double distance) {
+        this(distance, 0);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    public void initialize() {
+        timer.reset();
+        timer.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+
+        if (100 - ((shooter.getSpeed() / TARGET_RPM) * 100) <= PERCENT_THRESHOLD){
+            shooter.setInputSpeed(0.5);
+        }
+        else {
+            shooter.setInputSpeed(0);
+        }
+    }
+
+    private void setNetworkTable() {
+        velocityEntry.setDouble(shooter.getSpeed());
+    }
+
+    private double calculateVelocity(double distance) {
+        return (520.78 * Math.exp(0.1685 * distance));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return timeout > 0 && timer.get() >= timeout;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    public void end() {
+        timer.stop();
+        shooter.setSpeedRPM(0);
+        shooter.setInputSpeed(0);
+    }
+
+    // Called when another command which requires one or more of the same
+// subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
 }
