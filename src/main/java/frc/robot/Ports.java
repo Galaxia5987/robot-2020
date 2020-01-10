d0e5cad7812f0ec631e18e839936222309ccf0cb
Barel Shlidor <barel@shlidor.org>
2020-01-10 09:46:09 +0200

    Create Serializer's motors and proximities

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,6 +2,11 @@ package frc.robot;
 
 public class Ports {
     public static class Serializer {
-        
+        public static final int EXIT_MOTOR = 0;
+        public static final int ENTRY_MOTOR = 0;
+
+        public static final int ENTRY_PROXIMITY = 0;
+        public static final int MIDDLE_PROXIMITY = 0;
+        public static final int EXIT_PROXIMITY = 0;
     }
 }
--- src/main/java/frc/robot/subsystems/serializer/Serializer.java
+++ src/main/java/frc/robot/subsystems/serializer/Serializer.java
@@ -1,8 +1,19 @@
 package frc.robot.subsystems.serializer;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.Ports.Serializer.*;
+
 public class Serializer extends SubsystemBase {
+    private TalonSRX serializerExitMotor = new TalonSRX(EXIT_MOTOR);
+    private VictorSPX serializerEntryMotor = new VictorSPX(ENTRY_MOTOR);
+    private AnalogInput entryProximity = new AnalogInput(ENTRY_PROXIMITY);
+    private AnalogInput middleProximity = new AnalogInput(MIDDLE_PROXIMITY);
+    private AnalogInput exitProximity = new AnalogInput(EXIT_PROXIMITY);
+
     public Serializer() {
 
     }
