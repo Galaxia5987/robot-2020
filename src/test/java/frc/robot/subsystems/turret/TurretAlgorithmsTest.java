9b76e215755e517cf482039c2fbbcfe6803c47fa
Adam <adam.vichik@gmail.com>
2020-01-11 17:32:32 +0200

    Change setTurretAngle tests

--- src/test/java/frc/robot/subsystems/turret/TurretAlgorithmsTest.java
+++ src/test/java/frc/robot/subsystems/turret/TurretAlgorithmsTest.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.turret;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -15,26 +14,17 @@ public class TurretAlgorithmsTest {
         double currentPosition = -40;
         double minPos = -359;
         double maxPos = 359;
-
-        Assert.assertEquals(algorithmsTest.setTurretAngle(targetAngle,currentPosition, minPos, maxPos), -80, 0.1);
-        Assert.assertEquals(algorithmsTest.setTurretAngle(-40,30, minPos, maxPos), -40, 0.1);
-        Assert.assertEquals(algorithmsTest.setTurretAngle(300,-40, minPos, maxPos), -60, 0.1);
-        Assert.assertEquals(algorithmsTest.setTurretAngle(20,320, minPos, maxPos), 20, 0.1);
-        Assert.assertEquals(algorithmsTest.setTurretAngle(320,20, minPos, maxPos), -40, 0.1);
-        Assert.assertEquals(algorithmsTest.setTurretAngle(173,-247, minPos, maxPos), -187, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(targetAngle, currentPosition, minPos, maxPos), -80, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(-359, 200, minPos, maxPos), 1, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(300, -40, minPos, maxPos), -60, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(20, 320, minPos, maxPos), 20, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(320, 20, minPos, maxPos), -40, 0.1);
+        Assert.assertEquals(algorithmsTest.setTurretAngle(173, -247, minPos, maxPos), -187, 0.1);
     }
 
-    @Test
-    public void constrain() {
-        double targetAngle = 700;
-        double minPos = -359;
-        double maxPos = 359;
-
-        Assert.assertEquals(algorithmsTest.constrain(minPos, targetAngle, maxPos), 359, 2);
-    }
 
     @Test
-    public void center(){
+    public void center() {
         Assert.assertEquals(algorithmsTest.center(280, -359, 300), -80, 0.1);
     }
 }
