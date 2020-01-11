98f926c144a96627df35e7de2cde388f0d2a13ad
lior <lior5858@gmail.com>
2020-01-11 17:29:19 +0200

    Create  a basic PositionControl command

--- src/main/java/frc/robot/subsystems/ColorWheel/Commands/PositionControl.java
+++ src/main/java/frc/robot/subsystems/ColorWheel/Commands/PositionControl.java
@@ -1,11 +1,35 @@
 package frc.robot.subsystems.ColorWheel.Commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import static frc.robot.RobotContainer.colorWheel;
 
 public class PositionControl extends CommandBase {
 
-    public PositionControl(){
+    private String[] colors = {"Yellow", "Red", "Green", "Blue"};
+    private String currentColor;
+    private int currentIndex = 0;
+    private double spinCounter = 0.125;
+    private String gameData;
+    private String targetColor;
 
+    public PositionControl(){
+        gameData = DriverStation.getInstance().getGameSpecificMessage();
+        if (gameData.length() > 0){
+            switch (gameData.charAt(0)){
+                case('Y'):
+                    targetColor = "Yellow";
+                case('R'):
+                    targetColor = "Red";
+                case('G'):
+                    targetColor = "Green";
+                case('B'):
+                    targetColor = "Blue";
+                default:
+                    targetColor = "Unknown";
+            }
+        }
     }
 
     @Override
@@ -20,11 +44,11 @@ public class PositionControl extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return false;
+        return colorWheel.getColorString().equals(colors[colorWheel.indexOfColor(targetColor, targetColor)+2]);
     }
 
     @Override
     public void end(boolean interrupted) {
-        
+
     }
 }
