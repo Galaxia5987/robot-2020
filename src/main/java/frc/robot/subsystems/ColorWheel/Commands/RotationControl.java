81a405c90f322e90fc4821e66218a6876cb7ac62
lior <lior5858@gmail.com>
2020-01-11 17:11:41 +0200

    Now properly finishes a spin at the same color it started

--- src/main/java/frc/robot/subsystems/ColorWheel/Commands/RotationControl.java
+++ src/main/java/frc/robot/subsystems/ColorWheel/Commands/RotationControl.java
@@ -8,6 +8,7 @@ public class RotationControl extends CommandBase {
     private String[] colors = {"Yellow","Red","Green","Blue"};
     private String initialColor;
     private int initialIndex = 0;
+    private double spinCounter = 0.125;
 
     @Override
     public void initialize() {
