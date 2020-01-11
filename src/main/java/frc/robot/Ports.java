6a12536a078a704468a8e6fc5a0f8369a371a9c1
Unknown <orel.david1502@gmail.com>
2020-01-11 20:33:51 +0200

    Change the solenoids' ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,7 +4,7 @@ public class Ports {
     public class climber {
         public static final int LEFT_MOTOR = 0;
         public static final int RIGHT_MOTOR = 0;
-        public static final int STOPPER_FORWARD = 0;
-        public static final int STOPPER_REVERSE = 0;
+        public static final int STOPPER_FORWARD = 1;
+        public static final int STOPPER_REVERSE = 2;
     }
 }
