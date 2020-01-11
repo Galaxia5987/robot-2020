62b906bf36810fc7721a8af56b314ee854bc69d4
lior <lior5858@gmail.com>
2020-01-11 17:11:05 +0200

    Create a positionControl command

---
+++ src/main/java/frc/robot/subsystems/ColorWheel/Commands/PositionControl.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.ColorWheel.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class PositionControl extends CommandBase {
+
+    public PositionControl(){
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        
+    }
+}
