391372691a1ae3d8f3bb7e346b283d7d8cef1cd9
Adam <adam.vichik@gmail.com>
2020-01-11 17:45:23 +0200

    Create CenterTurret methods

--- src/main/java/frc/robot/subsystems/turret/commands/CenterTurret.java
+++ src/main/java/frc/robot/subsystems/turret/commands/CenterTurret.java
@@ -2,12 +2,35 @@ package frc.robot.subsystems.turret.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import static frc.robot.Constants.Turret.*;
 import static frc.robot.Robot.turret;
 
 public class CenterTurret extends CommandBase {
+    private double currentPosition;
 
     public CenterTurret() {
         addRequirements(turret);
     }
 
+    @Override
+    public void initialize() {
+        currentPosition = turret.getEncoderPosition();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+        turret.center(MAXIMUM_POSITION, MINIMUM_POSITION);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return Math.abs(turret.getEncoderPosition() - currentPosition) <= ANGLE_THRESHOLD;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        turret.stop();
+    }
 }
