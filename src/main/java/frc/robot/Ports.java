a44ca632cd5396cfc94e7e70221fd796dceb7343
Barel Shlidor <barel@shlidor.org>
2020-01-12 19:24:50 +0200

    Change ports names

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,9 +2,9 @@ package frc.robot;
 
 public final class Ports {
     public static final class Intake {
-        public static final int MASTER = 0;
+        public static final int INTAKE = 0;
         //TODO: Check, may need to switch between the channels
-        public static final int SOLENOID_FORWARD_CHANNEL = 0;
-        public static final int SOLENOID_BACKWARD_CHANNEL = 0;
+        public static final int FOLD_SOLENOID_FORWARD = 0;
+        public static final int FOLD_SOLENOID_REVERSE = 0;
     }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,13 +10,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.intake.Intake;
-
-import java.util.Set;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
