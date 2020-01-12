00088e3797ce5561115ad88222a500b95ab745a9
Barel Shlidor <barel@shlidor.org>
2020-01-12 16:38:31 +0200

    Rename Drop Balls command

--- src/main/java/frc/robot/subsystems/serializer/commands/MoveBallsToEntry.java
+++ src/main/java/frc/robot/subsystems/serializer/commands/MoveBallsToEntry.java
@@ -4,27 +4,32 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import static frc.robot.Constants.Serializer.BALL_DROP_TIME;
+import static frc.robot.Constants.Serializer.MAX_BALLS_COUNT;
 import static frc.robot.RobotContainer.serializer;
 
-public class DropBalls extends CommandBase {
+public class MoveBallsToEntry extends CommandBase {
     private int balls;
     private Timer timer = new Timer();
 
-    public DropBalls(int balls) {
+    public MoveBallsToEntry(int balls) {
         addRequirements(serializer);
         this.balls = balls;
+    }
+
+    public MoveBallsToEntry() {
+        this(MAX_BALLS_COUNT);
     }
 
     @Override
     public void initialize() {
         timer.reset();
         timer.start();
-        serializer.drop();
+        serializer.minimizeConveyor();
     }
 
     @Override
     public void execute() {
-        serializer.drop();
+        serializer.minimizeConveyor();
     }
 
     @Override
