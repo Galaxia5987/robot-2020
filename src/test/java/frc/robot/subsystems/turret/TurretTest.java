7a968b7f106df0cc3f19038ebea73d11c52b889f
Adam <adam.vichik@gmail.com>
2020-01-12 17:05:11 +0200

    Change test names

--- src/test/java/frc/robot/subsystems/turret/TurretTest.java
+++ src/test/java/frc/robot/subsystems/turret/TurretTest.java
@@ -13,25 +13,25 @@ public class TurretTest {
     }
 
     @Test
-    public void getCorrectionPosition() {
+    public void getNearestTurretPosition() throws Exception {
         double targetAngle = 280;
         double currentPosition = 30;
         double minPos = -360;
         double maxPos = 360;
-        Assert.assertEquals(turret.getCorrectPosition(targetAngle, currentPosition, minPos, maxPos), turret.convertDegreesToTicks(-80), 0.1);
-        Assert.assertEquals(turret.getCorrectPosition(-359, 200, minPos, maxPos), turret.convertDegreesToTicks(1), 0.1);
-        Assert.assertEquals(turret.getCorrectPosition(300, -40, minPos, maxPos), turret.convertDegreesToTicks(-60), 0.1);
-        Assert.assertEquals(turret.getCorrectPosition(20, 320, minPos, maxPos), turret.convertDegreesToTicks(20), 0.1);
-        Assert.assertEquals(turret.getCorrectPosition(320, 20, minPos, maxPos), turret.convertDegreesToTicks(-40), 0.1);
-        Assert.assertEquals(turret.getCorrectPosition(173, -247, minPos, maxPos), turret.convertDegreesToTicks(-187), 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(targetAngle, currentPosition, minPos, maxPos), turret.convertDegreesToTicks(-80), 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(-359, 200, minPos, maxPos), turret.convertDegreesToTicks(1), 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(300, -40, minPos, maxPos), turret.convertDegreesToTicks(-60), 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(20, 320, minPos, maxPos), turret.convertDegreesToTicks(20), 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(320, 20, minPos, maxPos), turret.convertDegreesToTicks(-40), 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(173, -247, minPos, maxPos), turret.convertDegreesToTicks(-187), 0.1);
     }
 
 
     @Test
-    public void getCorrectCenterPosition() {
+    public void center() {
         double currentPos = 200;
         double minPos = -360;
         double maxPos = 360;
-        Assert.assertEquals(turret.getCorrectCenterPosition(currentPos, minPos, maxPos), turret.convertDegreesToTicks(-160), 0.1);
+        Assert.assertEquals(turret.center(currentPos, minPos, maxPos), turret.convertDegreesToTicks(-160), 0.1);
     }
 }
