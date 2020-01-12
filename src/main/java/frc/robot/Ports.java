59dfbfc6b2c90673f2c69239d9ae00b4bd5e043e
Paulo Khayat <paulokhayat@gmail.com>
2020-01-12 21:56:48 +0200

    Revert "Merge pull request #26 from pauloalbert/shooter"
    This reverts commit 6eaeca96d145a60446f796919b8237cd51591c8d.

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,14 +1,4 @@
 package frc.robot;
 
 public class Ports {
-
-    public static class Shooter {
-        public static final int MASTER = 0;
-        public static final int SLAVE = 0;
-        public static final int INPUT_MOTOR = 0;
-        public static final boolean IS_MASTER_INVERTED = false;
-        public static final boolean IS_SLAVE_INVERTED = false;
-        public static final boolean SLAVE_SENSOR_PHASED = false;
-        public static final boolean MASTER_SENSOR_PHASED = false;
-    }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.shooter.Shooter;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -24,7 +23,6 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final Shooter shooter = new Shooter();
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
 
