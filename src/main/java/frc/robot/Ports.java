f532cc3060297992a67450b38e49c62de145e2dd
Barel Shlidor <barel@shlidor.org>
2020-01-14 16:40:40 +0200

    Apply ports conventions

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public class Ports {
     public static class Serializer {
-        public static final int EXIT_MOTOR = 0;
-        public static final int ENTRY_MOTOR = 0;
+        public static final int EXIT_MOTOR = 22;
+        public static final int ENTRY_MOTOR = 21;
 
         public static final int ENTRY_PROXIMITY = 0;
         public static final int INTEGRATION_PROXIMITY = 0;
