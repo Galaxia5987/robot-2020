0b6bfc0afc8f231c941aba7fdd944571262fc4df
Barel Shlidor <barel@shlidor.org>
2020-01-14 20:53:11 +0200

    Change files to conveyor instead serializer

--- src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
@@ -1,18 +1,18 @@
-package frc.robot.subsystems.serializer.commands;
+package frc.robot.subsystems.conveyor.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import static frc.robot.Constants.Serializer.BALL_FEED_TIME;
-import static frc.robot.Constants.Serializer.MAX_BALLS_COUNT;
-import static frc.robot.RobotContainer.serializer;
+import static frc.robot.Constants.Conveyor.BALL_FEED_TIME;
+import static frc.robot.Constants.Conveyor.MAX_BALLS_COUNT;
+import static frc.robot.RobotContainer.conveyor;
 
 public class FeedTurret extends CommandBase {
     public int ballsCount;
     private Timer timer = new Timer();
 
     public FeedTurret(int ballsCount) {
-        addRequirements(serializer);
+        addRequirements(conveyor);
         this.ballsCount = ballsCount;
     }
 
@@ -24,12 +24,12 @@ public class FeedTurret extends CommandBase {
     public void initialize() {
         timer.reset();
         timer.start();
-        serializer.feed();
+        conveyor.feed();
     }
 
     @Override
     public void execute() {
-        serializer.feed();
+        conveyor.feed();
     }
 
     @Override
@@ -40,6 +40,6 @@ public class FeedTurret extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         timer.stop();
-        serializer.stop();
+        conveyor.stop();
     }
 }
--- src/main/java/frc/robot/subsystems/conveyor/commands/MoveBallsToEntry.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/MoveBallsToEntry.java
@@ -1,18 +1,18 @@
-package frc.robot.subsystems.serializer.commands;
+package frc.robot.subsystems.conveyor.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import static frc.robot.Constants.Serializer.BALL_DROP_TIME;
-import static frc.robot.Constants.Serializer.MAX_BALLS_COUNT;
-import static frc.robot.RobotContainer.serializer;
+import static frc.robot.Constants.Conveyor.BALL_DROP_TIME;
+import static frc.robot.Constants.Conveyor.MAX_BALLS_COUNT;
+import static frc.robot.RobotContainer.conveyor;
 
 public class MoveBallsToEntry extends CommandBase {
     private int balls;
     private Timer timer = new Timer();
 
     public MoveBallsToEntry(int balls) {
-        addRequirements(serializer);
+        addRequirements(conveyor);
         this.balls = balls;
     }
 
@@ -24,12 +24,12 @@ public class MoveBallsToEntry extends CommandBase {
     public void initialize() {
         timer.reset();
         timer.start();
-        serializer.minimizeConveyor();
+        conveyor.minimizeConveyor();
     }
 
     @Override
     public void execute() {
-        serializer.minimizeConveyor();
+        conveyor.minimizeConveyor();
     }
 
     @Override
@@ -40,6 +40,6 @@ public class MoveBallsToEntry extends CommandBase {
     @Override
     public void end(boolean interrupted) {
         timer.stop();
-        serializer.stop();
+        conveyor.stop();
     }
 }
