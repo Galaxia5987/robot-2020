076133e89c4315ce0fc3df13212b36022ed48c3d
lior <lior5858@gmail.com>
2020-01-15 17:47:44 +0200

    Add a timer for the end phase of the position control

--- src/main/java/frc/robot/subsystems/color_wheel/commands/PositionControl.java
+++ src/main/java/frc/robot/subsystems/color_wheel/commands/PositionControl.java
@@ -1,15 +1,20 @@
 package frc.robot.subsystems.color_wheel.commands;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 
+import static frc.robot.Constants.ColorWheel.*;
 import static frc.robot.RobotContainer.colorWheel;
 
 public class PositionControl extends CommandBase {
 
     private final String[] colors = {"Yellow", "Red", "Green", "Blue"};
     private String targetColor;
+    private int currentColor;
+    private Timer endTimer = new Timer();
 
     public PositionControl() {
         String gameData = DriverStation.getInstance().getGameSpecificMessage();
@@ -32,6 +37,7 @@ public class PositionControl extends CommandBase {
     @Override
     public void initialize() {
         colorWheel.setMotorSpeed(Constants.ColorWheel.POSITION_SPEED);
+        endTimer.reset();
     }
 
     @Override
@@ -41,12 +47,16 @@ public class PositionControl extends CommandBase {
             colorWheel.setMotorSpeed(POSITION_SPEED * (distanceFromTarget * kP));
         else
             colorWheel.setMotorSpeed(-POSITION_SPEED * (distanceFromTarget * kP));
+        if (distanceFromTarget == 0){
+            endTimer.start();
+        }
     }
 
     @Override
     public boolean isFinished() {
         try {
-            return targetColor.equals(colors[colorWheel.indexOfColor(colorWheel.getColorString()) + Constants.ColorWheel.TILES_BEFORE_SENSOR]);
+            return targetColor.equals(colors[colorWheel.indexOfColor(colorWheel.getColorString()) + Constants.ColorWheel.TILES_BEFORE_SENSOR])
+                    && endTimer.get() > 4;
         } catch (Exception e) {
             return false;
         }
@@ -54,12 +64,16 @@ public class PositionControl extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
+        endTimer.stop();
         colorWheel.setMotorSpeed(0);
+        colorWheel.setNeutralMode(NeutralMode.Brake);
+    }
+
     public void getCurrentColor() {
         try {
             currentColor = colorWheel.indexOfColor(colorWheel.getColorString());
         } catch (Exception ignored) {
 
+        }
     }
-}
 }
