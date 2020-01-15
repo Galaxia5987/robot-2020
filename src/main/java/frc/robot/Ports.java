d9b4aa6d61516ba1a659f728472a70ff91868884
Adam <adam.vichik@gmail.com>
2020-01-15 18:16:38 +0200

    Implement port format

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -10,8 +10,8 @@ public class Ports {
         public static final boolean IS_ENCODER_INVERTED  = false;
     }
     public static class Shooter {
-        public static final int MASTER = 0;
-        public static final int SLAVE = 0;
+        public static final int MASTER = 51;
+        public static final int SLAVE = 52;
         public static final boolean IS_MASTER_INVERTED = false;
         public static final boolean IS_MASTER_ENCODER_INVERTED = false;
         public static final boolean IS_SLAVE_INVERTED = false;
