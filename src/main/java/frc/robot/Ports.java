9070bc572d72a7e6721517f639892191375ef813
lior <lior5858@gmail.com>
2020-01-16 12:54:51 +0200

    Update ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,10 +2,10 @@ package frc.robot;
 
 public class Ports {
     public class Drivetrain {
-        public static final int RIGHT_MASTER = 0;
-        public static final int RIGHT_SLAVE = 0;
-        public static final int LEFT_MASTER = 0;
-        public static final int LEFT_SLAVE = 0;
+        public static final int LEFT_MASTER = 11;
+        public static final int LEFT_SLAVE = 12;
+        public static final int RIGHT_MASTER = 13;
+        public static final int RIGHT_SLAVE = 14;
 
         public static final int SHIFTER_FORWARD_PORT = 0;
         public static final int SHIFTER_REVERSE_PORT = 0;
