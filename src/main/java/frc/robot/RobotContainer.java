9e5aee37bc98180f0db5fd8fe8db76e09e0bf6bd
Repulser <thetechproject2014@gmail.com>
2020-01-19 16:48:26 +0200

    Finish firelog

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import org.techfire225.webapp.Webserver;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -22,9 +23,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
+  private static final boolean debug = true;
 
 
   /**
@@ -33,6 +34,13 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+    if(debug) {
+      try {
+        new Webserver();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
   }
 
   /**
--- src/main/java/org/techfire225/webapp/Webserver.java
+++ src/main/java/org/techfire225/webapp/Webserver.java
@@ -1,31 +1,31 @@
 package org.techfire225.webapp;
 
+import edu.wpi.first.wpilibj.Filesystem;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
-import edu.wpi.first.wpilibj.Filesystem;
 import java.io.File;
 
 public class Webserver {
     public Webserver() throws Exception {
         Server server = new Server(5801);
-        
+
         ServletContextHandler servlets = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        
+
         servlets.addServlet(RedirectApplet.class, "/");
 
         servlets.addServlet(StateSocket.class, "/state/socket");
         servlets.addServlet(StateApplets.LatestState.class, "/state/latest");
-        
+
         ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setWelcomeFiles(new String[]{ "firescope.html" });
-        resource_handler.setResourceBase(new File(getClass().getResource("/www2").toURI()).getPath());
+        resource_handler.setWelcomeFiles(new String[]{"firescope.html"});
+        resource_handler.setResourceBase(new File(Filesystem.getDeployDirectory(), "firelog").getPath());
 
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] { resource_handler, servlets });
+        handlers.setHandlers(new Handler[]{resource_handler, servlets});
         server.setHandler(handlers);
 
         server.start();
