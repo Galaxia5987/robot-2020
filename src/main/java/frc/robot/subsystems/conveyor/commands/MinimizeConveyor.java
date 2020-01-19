4e34dc83f76fb20134353ebb472969f1dfbadb2e
Barel Shlidor <barel@shlidor.org>
2020-01-19 20:05:12 +0200

    Use CONVEYOR_MOTOR_RETURN_VELOCITY constant

--- src/main/java/frc/robot/subsystems/conveyor/commands/MinimizeConveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/MinimizeConveyor.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.conveyor.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import static frc.robot.RobotContainer.conveyor;
+import static frc.robot.Constants.Conveyor.*;
 
 public class MinimizeConveyor extends CommandBase {
 
@@ -12,7 +13,7 @@ public class MinimizeConveyor extends CommandBase {
 
     @Override
     public void initialize() {
-        conveyor.moveConveyor(-1);
+        conveyor.setConveyorVelocity(CONVEYOR_MOTOR_RETURN_VELOCITY);
     }
 
     @Override
