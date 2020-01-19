54b264a02d3c4fe2fdc89ed85abf732c4ed8cb1f
Paulo Khayat <paulokhayat@gmail.com>
2020-01-19 20:46:03 +0200

    Create timer and utilize it

--- src/main/java/frc/robot/subsystems/conveyor/commands/MinimizeConveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/MinimizeConveyor.java
@@ -1,18 +1,23 @@
 package frc.robot.subsystems.conveyor.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import static frc.robot.RobotContainer.conveyor;
 import static frc.robot.Constants.Conveyor.*;
 
 public class MinimizeConveyor extends CommandBase {
-
-    public MinimizeConveyor() {
+    private Timer timer = new Timer();
+    private double timeout;
+    public MinimizeConveyor(double timeout) {
         addRequirements(conveyor);
+        this.timeout = timeout;
     }
 
     @Override
     public void initialize() {
+        timer.reset();
+        timer.start();
         conveyor.setConveyorSpeed(CONVEYOR_MOTOR_RETURN_VELOCITY);
     }
 
@@ -23,12 +28,13 @@ public class MinimizeConveyor extends CommandBase {
 
     @Override
     public boolean isFinished() {
-        return conveyor.feederSensedObject();
+        return conveyor.feederSensedObject() || timer.get() >= timeout || timeout == 0;
     }
 
     @Override
     public void end(boolean interrupted) {
         conveyor.stop();
+        timer.stop();
     }
 
 }
