8399c9366f96b2610cb97fd50457c3832a424ef5
lior <lior5858@gmail.com>
2020-01-20 22:28:12 +0200

    Correct unit model velocity error

--- src/main/java/frc/robot/valuetuner/ValueTuner.java
+++ src/main/java/frc/robot/valuetuner/ValueTuner.java
@@ -8,8 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.*;
 
 public class ValueTuner {
 
