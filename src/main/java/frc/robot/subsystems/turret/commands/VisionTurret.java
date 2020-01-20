6cc1215df5d7732244f894233c43e82eac8de191
Adam <adam.vichik@gmail.com>
2020-01-20 22:39:15 +0200

    Create VisionTurret

---
+++ src/main/java/frc/robot/subsystems/turret/commands/VisionTurret.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.turret.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.turret.Turret;
+
+public class VisionTurret extends CommandBase {
+    private Turret turret;
+//    private MiniPID anglePid = new MiniPID(VISION_KP, VISION_KI, VISION_KD);
+
+    public VisionTurret(Turret turret) {
+        addRequirements(turret);
+        this.turret = turret;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+//        anglePid.setPID(VISION_KP, VISION_KI, VISION_KD);
+//        turret.setAngle(anglePid.getOutput(turret.getVisionAngle(), 0));
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+//        return Math.abs(turret.getVisionAngle()) <= VISION_ANGLE_THRESHOLD;
+        return false;
+    }
+}
