7c8b632170f98f722a1631f457e46c882239028b
Adam <adam.vichik@gmail.com>
2020-01-20 20:07:19 +0200

    Create AutoShoot command group

--- src/main/java/frc/robot/commandgroups/AutoShoot.java
+++ src/main/java/frc/robot/commandgroups/AutoShoot.java
@@ -3,23 +3,24 @@ package frc.robot.commandgroups;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.commands.SpeedUp;
+import frc.robot.subsystems.shooter.commands.Wait;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.commands.TurnTurret;
 
-public class TurnAndShoot extends SequentialCommandGroup {
+public class AutoShoot extends ParallelCommandGroup {
 
-    public TurnAndShoot(Turret turret, Shooter shooter, double timeout) {
+    public AutoShoot(Turret turret, Shooter shooter, Conveyor conveyor, boolean gate, boolean stop) {
         addRequirements(turret, shooter);
         addCommands(
                 // turn the turret to the setpoint angle
                 // ready the flywheel to shoot the balls to the target distance for the desired amount of time
-                new ParallelCommandGroup(new TurnTurret(turret), new SpeedUp(shooter, timeout)),
-                // shoot the balls until the feed command ends
-                new ParallelDeadlineGroup(new FeedTurret(), new SpeedUp(shooter))
-        );
+                new TurnTurret(turret, stop),
+                new SpeedUp(shooter, stop),
+                new SequentialCommandGroup(new Wait(shooter, turret), new AutoFeed(conveyor, gate)));
     }
 
 }
