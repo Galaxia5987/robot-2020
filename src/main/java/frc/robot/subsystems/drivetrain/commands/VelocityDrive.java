db4db156f8771b73a12272df092ec8c313d78edc
lior <lior5858@gmail.com>
2020-01-20 17:27:39 +0200

    Create VelocityDrive command

---
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/VelocityDrive.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+/**
+ *
+ */
+public class VelocityDrive extends CommandBase {
+
+    public VelocityDrive(Drivetrain drivetrain) {
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    public void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+
+    public void end(boolean interrupted) {
+    }
+
+}
