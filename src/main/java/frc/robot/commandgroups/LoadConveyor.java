3de9d74f4b73f8bc518066378b3cdfd5f4d3e025
Adam <adam.vichik@gmail.com>
2020-01-20 18:09:58 +0200

    Change LoadConveyor

--- src/main/java/frc/robot/commandgroups/LoadConveyor.java
+++ src/main/java/frc/robot/commandgroups/LoadConveyor.java
@@ -4,18 +4,19 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
+import frc.robot.subsystems.conveyor.commands.Gate;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.IntakePowerCell;
-import frc.robot.subsystems.intake.commands.OuttakeBall;
+import frc.robot.subsystems.intake.commands.ToggleIntake;
 
 public class LoadConveyor extends SequentialCommandGroup {
 
-    public LoadConveyor(Intake intake, Conveyor conveyor, double speed, double timeout, int balls, int remainBalls){
+    public LoadConveyor(Intake intake, Conveyor conveyor, boolean down, boolean gate, double speed, double timeout){
         addRequirements(intake, conveyor);
         addCommands(
-                new IntakePowerCell(speed, timeout),
-                new ParallelCommandGroup(new OuttakeBall(speed, timeout), new FeedTurret(balls)),
-                new FeedTurret(remainBalls)
+                new ToggleIntake(down),
+                // move the power cells to the turret and
+                new ParallelCommandGroup(new IntakePowerCell(speed, timeout), new FeedTurret(), new Gate(conveyor, gate))
         );
     }
 
