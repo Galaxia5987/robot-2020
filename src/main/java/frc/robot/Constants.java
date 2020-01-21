15b682ef6a6629c6926f1e7c1a5c8e5a6d9e497a
Adam <adam.vichik@gmail.com>
2020-01-21 19:57:13 +0200

    Create vision constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,6 +37,11 @@ public final class Constants {
         public static double KD = 0;
         public static double KF = 0;
         public static final boolean STOP_TURRET = false;
+        public static final double VISION_ANGLE_THRESHOLD = 1;
+        public static final double VISION_KP = 0;
+        public static final double VISION_KI = 0;
+        public static final double VISION_KD = 0;
+        public static final double VISION_SETPOINT = 0;
       }
 
     static { // Runs alongside main
