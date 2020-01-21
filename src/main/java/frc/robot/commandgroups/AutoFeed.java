444671869aaf99f84324157ea905415c0f3bfd39
Adam <adam.vichik@gmail.com>
2020-01-21 20:20:56 +0200

    Create openGate and closeGate and utilize them

--- src/main/java/frc/robot/commandgroups/AutoFeed.java
+++ src/main/java/frc/robot/commandgroups/AutoFeed.java
@@ -1,11 +1,9 @@
 package frc.robot.commandgroups;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
-import frc.robot.subsystems.conveyor.commands.MoveGate;
-
-import static frc.robot.Constants.Conveyor.OPEN_GATE;
 
 public class AutoFeed extends ParallelCommandGroup {
 
@@ -13,7 +11,7 @@ public class AutoFeed extends ParallelCommandGroup {
         addRequirements(conveyor);
         addCommands(
                 // move the gate and let the power cells move into the turret
-                new MoveGate(conveyor, OPEN_GATE),
+                new InstantCommand(conveyor::openGate, conveyor),
                 // feed the power cells to the turret
                 new FeedTurret()
         );
--- src/main/java/frc/robot/commandgroups/LoadConveyor.java
+++ src/main/java/frc/robot/commandgroups/LoadConveyor.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
-import frc.robot.subsystems.conveyor.commands.MoveGate;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.IntakePowerCell;
 import frc.robot.subsystems.intake.commands.ToggleIntake;
