f5d3748e7a12a890d11837d6eb38c4e5de531755
Adam <adam.vichik@gmail.com>
2020-01-21 16:34:41 +0200

    Make Gate an Instant command

--- src/main/java/frc/robot/subsystems/conveyor/commands/Gate.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/Gate.java
@@ -1,32 +1,27 @@
 package frc.robot.subsystems.conveyor.commands;
 
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.conveyor.Conveyor;
 
-public class Gate extends CommandBase {
+
+public class Gate extends InstantCommand {
     private boolean open;
-    private Solenoid gate;
+    private Conveyor conveyor;
 
     public Gate(Conveyor conveyor, boolean open){
         addRequirements(conveyor);
         this.open = open;
-        gate = conveyor.getGate();
+        this.conveyor = conveyor;
     }
 
     @Override
     public void initialize() {
-        gate.set(open);
+        conveyor.setGate(open);
     }
 
     @Override
     public void execute() {
 
-    }
-
-    @Override
-    public boolean isFinished() {
-        return open == gate.get();
     }
 
     @Override
