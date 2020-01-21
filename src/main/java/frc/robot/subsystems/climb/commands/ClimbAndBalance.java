f3cf1743ee1ffc431c01dfe2b4855a0cd0f1c218
oreld <orel.david1502@gmail.com>
2020-01-21 16:55:36 +0200

    Create a command group for climbing

---
+++ src/main/java/frc/robot/subsystems/climb/commands/ClimbAndBalance.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.climb.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.climb.Climber;
+
+public class ClimbAndBalance extends SequentialCommandGroup {
+
+    public ClimbAndBalance(Climber climber , double setpoint){
+        addRequirements(climber);
+        addCommands(
+                new RiseToHeight(climber, setpoint),
+                new BalanceRobot(climber , setpoint)
+        );
+    }
+
+}
