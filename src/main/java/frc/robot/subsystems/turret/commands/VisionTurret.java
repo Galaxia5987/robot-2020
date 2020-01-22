5bf8401c0e6bb463765cf5f3753beee6ef750fc5
oreld <orel.david1502@gmail.com>
2020-01-22 20:46:47 +0200

    Create VisionTurret code

--- src/main/java/frc/robot/subsystems/turret/commands/VisionTurret.java
+++ src/main/java/frc/robot/subsystems/turret/commands/VisionTurret.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.turret.commands;
 
+import com.stormbots.MiniPID;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.turret.Turret;
 
+import static frc.robot.Constants.Turret.*;
+
 public class VisionTurret extends CommandBase {
     private Turret turret;
-//    private MiniPID anglePid = new MiniPID(VISION_KP, VISION_KI, VISION_KD);
+    private MiniPID anglePid = new MiniPID(VISION_KP, VISION_KI, VISION_KD);
 
     public VisionTurret(Turret turret) {
         addRequirements(turret);
@@ -20,18 +23,17 @@ public class VisionTurret extends CommandBase {
     // Called repeatedly when this Command is scheduled to run
     @Override
     public void execute() {
-//        anglePid.setPID(VISION_KP, VISION_KI, VISION_KD);
-//        turret.setAngle(anglePid.getOutput(turret.getVisionAngle(), 0));
+        anglePid.setPID(VISION_KP, VISION_KI, VISION_KD);
+        turret.setAngle(anglePid.getOutput(turret.getVisionAngle(), VISION_SETPOINT));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return Math.abs(VISION_SETPOINT - turret.getVisionAngle()) <= VISION_ANGLE_THRESHOLD;
     }
 
     @Override
     public void end(boolean interrupted) {
         turret.stop();
-    }
-
-    @Override
-    public boolean isFinished() {
-//        return Math.abs(turret.getVisionAngle()) <= VISION_ANGLE_THRESHOLD;
-        return false;
     }
 }
