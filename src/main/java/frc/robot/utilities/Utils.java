71abb6aa5efff70131869557ea598afa813a6382
adam <adam.vichik@gmail.com>
2020-01-22 21:01:22 +0200

    Move FloorMod to Utils

--- src/main/java/frc/robot/utilities/Utils.java
+++ src/main/java/frc/robot/utilities/Utils.java
@@ -22,5 +22,20 @@ public class Utils {
     public static void setValue(NetworkTableEntry entry, double value) {
         entry.setValue(value);
     }
-    
+
+
+    /**
+     * recreates the results of Math.floorMod() for Double type variables.
+     * The result is the unsigned remainder of the mod method.
+     *
+     * @param value the numerator
+     * @param mod the denominator
+     * @return the remainder of the division
+     */
+    public static double floorMod(double value, double mod) {
+        value %= mod;
+        value += mod;
+        value %= mod;
+        return value;
+    }
 }
