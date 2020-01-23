cb66fcd826ea83ad46a3edcdfa969385fac8256a
oreld <orel.david1502@gmail.com>
2020-01-23 19:04:50 +0200

    Fix ports numbers

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,8 +2,8 @@ package frc.robot;
 
 public class Ports {
     public class climber {
-        public static final int LEFT_MOTOR = 31;
-        public static final int RIGHT_MOTOR = 32;
+        public static final int LEFT_MOTOR = 61;
+        public static final int RIGHT_MOTOR = 62;
         public static final int STOPPER_FORWARD = 1;
         public static final int STOPPER_REVERSE = 2;
     }
