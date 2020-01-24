44398d84262e5e4cf386ab7934096b42e76d5a0c
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-01-24 12:26:12 +0200

    Fix feed turret command

--- src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
@@ -3,35 +3,30 @@ package frc.robot.subsystems.conveyor.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.conveyor.Conveyor;
 
+import static frc.robot.Constants.Conveyor.CONVEYOR_MOTOR_FEED_VELOCITY;
 import static frc.robot.Constants.Conveyor.MAX_BALLS_AMOUNT;
 
 public class FeedTurret extends CommandBase {
     private Conveyor conveyor;
-    private int ballsLeft; //Shoot out X balls!
-
-    public FeedTurret(Conveyor conveyor, int balls) {
-        this.conveyor = conveyor;
-        addRequirements(conveyor);
-        this.ballsLeft = MAX_BALLS_AMOUNT - balls;
-    }
 
     public FeedTurret(Conveyor conveyor) {
-        this(conveyor, Math.max(MAX_BALLS_AMOUNT, conveyor.getBallsCount())); //In case there are more than 5..
+        this.conveyor = conveyor;
+        addRequirements(conveyor);
     }
 
     @Override
     public void initialize() {
-        conveyor.feed();
+        conveyor.openGate(true);
+        conveyor.setConveyorSpeed(CONVEYOR_MOTOR_FEED_VELOCITY);
     }
 
     @Override
     public void execute() {
-        conveyor.feed();
     }
 
     @Override
     public boolean isFinished() {
-        return conveyor.getBallsCount() <= ballsLeft;
+        return conveyor.getBallsCount() <= 0;
     }
 
     @Override
