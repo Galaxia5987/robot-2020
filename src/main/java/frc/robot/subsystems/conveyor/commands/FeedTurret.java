785d2b93abe7558044e378d69fab893a33d04c59
Paulo Khayat <paulokhayat@gmail.com>
2020-01-24 12:11:53 +0200

    Erroneous

--- src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
@@ -1,19 +1,24 @@
 package frc.robot.subsystems.conveyor.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.conveyor.Conveyor;
 
 import static frc.robot.Constants.Conveyor.MAX_BALLS_AMOUNT;
 import static frc.robot.RobotContainer.conveyor;
 
 public class FeedTurret extends CommandBase {
+    private Conveyor conveyor;
     private int ballsLeft; //Shoot out X balls!
 
-    public FeedTurret(int balls) {
+    public FeedTurret(Conveyor conveyor, int balls) {
+        this.conveyor = conveyor;
         addRequirements(conveyor);
         this.ballsLeft = MAX_BALLS_AMOUNT - balls;
     }
 
     public FeedTurret() {
+        
         this(Math.max(MAX_BALLS_AMOUNT, conveyor.getBallsCount())); //In case there are more than 5..
     }
 
