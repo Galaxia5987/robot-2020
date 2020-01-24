a98872283822ead7e7e88277baff4906ced754db
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-01-24 14:01:30 +0200

    Update CAN bus

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,7 +3,7 @@ package frc.robot;
 
 public class Ports {
     public static class Conveyor {
-        public static final int MOTOR = 22;
+        public static final int MOTOR = 21;
 
         public static final int FEEDER_PROXIMITY = 4;
         public static final int CONVEYOR_PROXIMITY = 1;
@@ -14,21 +14,21 @@ public class Ports {
     public static final int TALON_PID_SLOT = 0;
 
     public static final class Intake {
-        public static final int MASTER = 11;
+        public static final int MASTER = 20;
         //TODO: Check, may need to switch between the channels
         public static final int FOLD_SOLENOID_FORWARD = 6;
         public static final int FOLD_SOLENOID_REVERSE = 7;
     }    
 
     public static class Turret {
-        public static final int MOTOR = 41;
+        public static final int MOTOR = 22;
         public static final boolean IS_MOTOR_INVERTED = false;
         public static final boolean IS_ENCODER_INVERTED  = false;
     }
     public static class Shooter {
-        public static final int MASTER = 51;
-        public static final int SLAVE_1 = 52;
-        public static final int SLAVE_2 = 52;
+        public static final int MASTER = 23;
+        public static final int SLAVE_1 = 24;
+        public static final int SLAVE_2 = 25;
         public static final boolean IS_MASTER_INVERTED = false;
         public static final boolean IS_MASTER_ENCODER_INVERTED = false;
         public static final boolean IS_SLAVE_1_INVERTED = false;
