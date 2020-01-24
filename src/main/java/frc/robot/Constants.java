791034991fc6bbca99def02439e829ce0f3fce3c
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-01-24 13:29:01 +0200

    Remove unused constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,7 +26,6 @@ public final class Constants {
         public static final int TICKS_PER_DEGREE = 1;
         public static final int MOTION_MAGIC_CRUISE_VELOCITY = 0;
         public static final int MOTION_MAGIC_ACCELERATION = 0;
-        public static final double HALL_EFFECT_POSITION_1 = 0; // in degrees, the two different positions are if the turret has turned a full circle or not
         public static final double ANGLE_THRESHOLD = 1;
         public static final double MINIMUM_POSITION = -200;
         public static final double MAXIMUM_POSITION = 200;
