d7259c2858332193c5c7a3c91ab176cd3bf0a89c
Adam <adam.vichik@gmail.com>
2020-01-25 16:51:41 +0200

    Utilize lambda command and add parameters

--- src/main/java/frc/robot/commandgroups/TurnAndShoot.java
+++ src/main/java/frc/robot/commandgroups/TurnAndShoot.java
@@ -1,8 +1,6 @@
 package frc.robot.commandgroups;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
 import frc.robot.subsystems.shooter.Shooter;
@@ -14,14 +12,15 @@ import static frc.robot.Constants.Conveyor.OPEN_GATE;
 
 public class TurnAndShoot extends SequentialCommandGroup {
     // for when there is no vision
-    public TurnAndShoot(Turret turret, Shooter shooter, Conveyor conveyor, double timeout) {
+    public TurnAndShoot(Turret turret, Shooter shooter, Conveyor conveyor, double timeout, double distance, double angle) {
         addRequirements(turret, shooter);
         addCommands(
                 // turn the turret to the setpoint angle
                 // ready the flywheel to shoot the balls to the target distance for the desired amount of time
-                new ParallelCommandGroup(new TurnTurret(turret), new SpeedUp(shooter).withTimeout(timeout)),
+                new ParallelCommandGroup(new TurnTurret(turret, angle), new SpeedUp(shooter, conveyor, distance).withTimeout(timeout)),
                 // shoot the balls until the feed command ends
-                new ParallelDeadlineGroup(new FeedTurret(conveyor, OPEN_GATE), new SpeedUp(shooter))
+                new FeedTurret(conveyor, OPEN_GATE),
+                new RunCommand(shooter::stop)
         );
     }
 
