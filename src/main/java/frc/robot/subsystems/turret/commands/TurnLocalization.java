3e3e7a0c59f361b569b50e33ca363ecb0aaef7a8
Adam <adam.vichik@gmail.com>
2020-01-26 18:34:13 +0200

    Add import

--- src/main/java/frc/robot/subsystems/turret/commands/TurnLocalization.java
+++ src/main/java/frc/robot/subsystems/turret/commands/TurnLocalization.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.turret.Turret;
 
+import static frc.robot.Constants.Field_Geometry.POWER_PORT_LOCATION;
+
 public class TurnLocalization extends CommandBase {
     private final Turret turret;
 
@@ -20,8 +22,8 @@ public class TurnLocalization extends CommandBase {
 
     private double calculateTargetAngle() {
         Pose2d localization = turret.getLocalization();
-        double deltaY = Math.abs(Constants.Field_Geometry.POWER_PORT_LOCATION.getTranslation().getY() - localization.getTranslation().getY());
-        double deltaX = Math.abs(Constants.Field_Geometry.POWER_PORT_LOCATION.getTranslation().getX() - localization.getTranslation().getX());
+        double deltaY = Math.abs(POWER_PORT_LOCATION.getTranslation().getY() - localization.getTranslation().getY());
+        double deltaX = Math.abs(POWER_PORT_LOCATION.getTranslation().getX() - localization.getTranslation().getX());
         return Math.toDegrees(Math.atan2(deltaY, deltaX) - localization.getRotation().getRadians());
     }
 }
