9ae990adafb046a3a959bdc014c4ec9466c981e2
Adam <adam.vichik@gmail.com>
2020-01-26 19:57:13 +0200

    Delete the joystick from the TurretSwitching

--- src/main/java/frc/robot/subsystems/turret/commands/TurretSwitching.java
+++ src/main/java/frc/robot/subsystems/turret/commands/TurretSwitching.java
@@ -4,14 +4,12 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.turret.Turret;
 
 public class TurretSwitching extends CommandBase {
-    private final JoystickTurret joystickTurret;
     private final TurnLocalization turnLocalization;
     private final VisionTurret visionTurret;
     private final Turret turret;
 
     public TurretSwitching(Turret turret) {
         this.turret = turret;
-        this.joystickTurret = new JoystickTurret(turret);
         this.turnLocalization = new TurnLocalization(turret);
         this.visionTurret = new VisionTurret(turret);
     }
