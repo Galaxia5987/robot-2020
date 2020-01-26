406b7410276596f01a1369911b9aedb0f30f24b8
lior <lior5858@gmail.com>
2020-01-26 16:20:32 +0200

    Update drivetrain ports to match robot ids

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,10 +2,10 @@ package frc.robot;
 
 public class Ports {
     public static class Drivetrain {
-        public static final int LEFT_MASTER = 0;
-        public static final int LEFT_SLAVE = 28;
-        public static final int RIGHT_MASTER = 1;
-        public static final int RIGHT_SLAVE = 2;
+        public static final int LEFT_MASTER = 1;
+        public static final int LEFT_SLAVE = 2;
+        public static final int RIGHT_MASTER = 3;
+        public static final int RIGHT_SLAVE = 4;
 
         public static final int SHIFTER_FORWARD_PORT = 0;
         public static final int SHIFTER_REVERSE_PORT = 0;
