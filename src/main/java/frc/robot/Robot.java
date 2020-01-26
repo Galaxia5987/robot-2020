22c8607e9922fccf4741e2d6c22456c06effc315
lior <lior5858@gmail.com>
2020-01-26 17:40:10 +0200

    Delete dumb import

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,7 +11,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import static frc.robot.RobotContainer.colorWheel;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
