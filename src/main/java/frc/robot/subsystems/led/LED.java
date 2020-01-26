159b863a846c42a2e7c9e45a8dd6879f05bbc113
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-01-26 21:13:59 +0200

    Create LED base code

---
+++ src/main/java/frc/robot/subsystems/led/LED.java
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2020 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+
+package frc.robot.subsystems.led;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Subsystem controlling the LEDs on the robot.
+ */
+public class LED extends SubsystemBase {
+    /**
+     * Creates a new LED subsystem.
+     */
+    public LED() {
+
+    }
+}
