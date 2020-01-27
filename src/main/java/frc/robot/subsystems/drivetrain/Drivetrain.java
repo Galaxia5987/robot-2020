99398443c0ff3416ccf119f51e8184ab75f14629
oreld <orel.david1502@gmail.com>
2020-01-27 22:58:15 +0200

    Create setPower

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems.drivetrain;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -90,6 +91,11 @@ public class Drivetrain extends SubsystemBase {
 
         localization = new FullLocalization( new Rotation2d(0),ROBOT_WIDTH);
 
+    }
+
+    public void setPower(double left, double right) {
+        leftMaster.set(ControlMode.PercentOutput, left);
+        rightMaster.set(ControlMode.PercentOutput, right);
     }
 
     public void shiftGear(shiftModes mode) {
