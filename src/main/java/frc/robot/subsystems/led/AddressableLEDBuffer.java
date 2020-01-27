a8286036c56fc6271c0577637ca7066523e5cf23
Dan Katzuv <dan.dan.kafe@gmail.com>
2020-01-27 09:34:44 +0200

    Add led buffer class to project to allow extendability

---
+++ src/main/java/frc/robot/subsystems/led/AddressableLEDBuffer.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019-2020 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.led;
+
+/**
+ * Buffer storage for Addressable LEDs.
+ */
+public class AddressableLEDBuffer extends edu.wpi.first.wpilibj.AddressableLEDBuffer {
+    byte[] m_buffer;
+
+    /**
+     * Constructs a new LED buffer with the specified length.
+     *
+     * @param length The length of the buffer in pixels
+     */
+    public AddressableLEDBuffer(int length) {
+        super(length);
+        m_buffer = new byte[length * 4];
+    }
+
+    /**
+     * Sets a specific led in the buffer.
+     *
+     * @param index the index to write
+     * @param r     the r value [0-255]
+     * @param g     the g value [0-255]
+     * @param b     the b value [0-255]
+     */
+    @SuppressWarnings("ParameterName")
+    public void setRGB(int index, int r, int g, int b) {
+        m_buffer[index * 4] = (byte) b;
+        m_buffer[(index * 4) + 1] = (byte) g;
+        m_buffer[(index * 4) + 2] = (byte) r;
+        m_buffer[(index * 4) + 3] = 0;
+    }
+}
--- src/main/java/frc/robot/subsystems/led/LED.java
+++ src/main/java/frc/robot/subsystems/led/LED.java
@@ -9,7 +9,6 @@
 package frc.robot.subsystems.led;
 
 import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
