4f01c2030deed8521e7e6f2ba87cdcdb05464c9e
Paulo Khayat <paulokhayat@gmail.com>
2020-01-27 22:10:02 +0200

    Fix TurnAndShoot

--- src/main/java/frc/robot/commandgroups/TurnAndShoot.java
+++ src/main/java/frc/robot/commandgroups/TurnAndShoot.java
@@ -5,20 +5,24 @@ import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.commands.SpeedUp;
+import frc.robot.subsystems.shooter.commands.WaitForShootingVision;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.commands.TurnTurret;
+import frc.robot.subsystems.turret.commands.VisionTurret;
 
-public class TurnAndShoot extends SequentialCommandGroup {
+/**
+ * This command group shoots Power Cells to a distance and angle which are predefined.
+ * This command will most likely be obsolete after testing, but it allows you to run all of the necessary functions
+ * for shooting.
+ */
+public class TurnAndShoot extends ParallelDeadlineGroup {
     // for when there is no vision
     public TurnAndShoot(Turret turret, Shooter shooter, Conveyor conveyor, double timeout, double distance, double angle) {
+        super(new SequentialCommandGroup(new WaitForShootingVision(shooter, turret), new FeedTurret(conveyor)));
         addRequirements(turret, shooter);
         addCommands(
-                // turn the turret to the setpoint angle
-                // ready the flywheel to shoot the balls to the target distance for the desired amount of time
-                new ParallelCommandGroup(new TurnTurret(turret, angle), new SpeedUp(shooter, conveyor, distance).withTimeout(timeout)),
-                // shoot the balls until the feed command ends
-                new FeedTurret(conveyor),
-                new RunCommand(shooter::stop)
+                new TurnTurret(turret, angle),
+                new SpeedUp(shooter, conveyor, distance).withTimeout(timeout)
         );
     }
 
