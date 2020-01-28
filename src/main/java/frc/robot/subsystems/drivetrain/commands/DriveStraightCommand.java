40feab944a7917f4afd9b581f692c52a40cfff63
oreld <orel.david1502@gmail.com>
2020-01-28 17:14:29 +0200

    Add methods to drive the drivetrain

---
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/DriveStraightCommand.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+
+import static frc.robot.Constants.Drivetrain.SHIFTER_COOLDOWN;
+
+/**
+ *
+ */
+public class DriveStraightCommand extends CommandBase {
+    private final Drivetrain drivetrain;
+    private double leftSpeed, rightSpeed;
+
+    /**
+     *
+     */
+    public DriveStraightCommand(Drivetrain drivetrain, double rightSpeed, double leftSpeed) {
+        this.drivetrain = drivetrain;
+        this.leftSpeed = leftSpeed;
+        this.rightSpeed = rightSpeed;
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        drivetrain.setPower(leftSpeed, rightSpeed);
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return Math.abs(RobotContainer.rightJoystick.getY()) > leftSpeed || Math.abs(RobotContainer.leftJoystick.getY()) > rightSpeed;
+    }
+}
