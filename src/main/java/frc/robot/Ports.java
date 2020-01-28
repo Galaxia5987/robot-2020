5c2df128172137d8c6c2b08ed2ab5946ef0532a3
oreld <orel.david1502@gmail.com>
2020-01-28 20:32:45 +0200

    Add victors to Ports.java

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,9 +3,11 @@ package frc.robot;
 public class Ports {
     public static class Drivetrain {
         public static final int LEFT_MASTER = 3;
-        public static final int LEFT_SLAVE = 28;
+        public static final int LEFT_SLAVE1 = 4;
+        public static final int LEFT_SLAVE2 = 5;
         public static final int RIGHT_MASTER = 6;
-        public static final int RIGHT_SLAVE = 2;
+        public static final int RIGHT_SLAVE1 = 7;
+        public static final int RIGHT_SLAVE2 = 8;
 
         public static final int SHIFTER_FORWARD_PORT = 0;
         public static final int SHIFTER_REVERSE_PORT = 0;
