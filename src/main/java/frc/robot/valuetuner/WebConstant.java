dc6587f39a7995b2935ea8b53f56782024077641
Repulser <thetechproject2014@gmail.com>
2020-01-30 16:08:09 +0200

    Clean up and document value tuner code

--- src/main/java/frc/robot/valuetuner/WebConstant.java
+++ src/main/java/frc/robot/valuetuner/WebConstant.java
@@ -5,22 +5,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static spark.Spark.post;
 
+/**
+ * This class holds all the key value constants that will show up in the value tuner
+ */
 public class WebConstant {
     private static Map<String, ConstantObject> constantMap = new ConcurrentHashMap<>();
     private final ConstantObject constant;
-
-    static {
-        post("/set", (request, response) -> {
-            String key = request.queryParamOrDefault("key", null);
-            String value = request.queryParamOrDefault("value", null);
-            ConstantObject constant = WebConstant.getConstantMap().get(key);
-            if (constant != null) {
-                constant.setValue(Double.parseDouble(value));
-            }
-            return "set";
-        });
-    }
-
 
     public WebConstant(String key, double value) {
         this.constant = new ConstantObject(key, value);
