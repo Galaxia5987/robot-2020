6a9833e560c44fa962b222670f30516da0c3049f
Repulser <thetechproject2014@gmail.com>
2020-01-30 16:35:38 +0200

    Fix build

--- src/main/java/frc/robot/subsystems/turret/commands/JoystickTurret.java
+++ src/main/java/frc/robot/subsystems/turret/commands/JoystickTurret.java
@@ -1,16 +1,12 @@
 package frc.robot.subsystems.turret.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.turret.Turret;
-import frc.robot.utilities.Utils;
 
-import static frc.robot.Constants.Turret.*;
-import static frc.robot.RobotContainer.*;
-
+import static frc.robot.Constants.Turret.TURRET_JOYSTICK_SPEED;
+import static frc.robot.RobotContainer.getRightXboxY;
 
 public class JoystickTurret extends CommandBase {
-    private RobotContainer m_robotContainer = new RobotContainer();
     private final Turret turret;
 
     public JoystickTurret(Turret turret) { //This does not power the motors, it simply adjusts the target angle. Henceforth, there is no reason to interrupt the turning class.
@@ -24,7 +20,7 @@ public class JoystickTurret extends CommandBase {
 
     @Override
     public void execute() {
-        double joystickInput = m_robotContainer.getXboxY();
+        double joystickInput = getRightXboxY();
         double offset = joystickInput * TURRET_JOYSTICK_SPEED;
         turret.setOffset(offset);
     }
