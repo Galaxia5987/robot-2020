b554f7534256d978b2b7dd1c033e90684909a8ee
Orel David Aboukasis <orel.david1502@gmail.com>
2020-01-31 13:28:52 +0200

    Create testing for the calculateTargetAngle method

--- src/test/java/frc/robot/subsystems/turret/TurretTest.java
+++ src/test/java/frc/robot/subsystems/turret/TurretTest.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems.turret;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.UnitModel;
+import frc.robot.subsystems.turret.commands.TurnLocalization;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,11 +13,13 @@ import static frc.robot.Constants.Turret.TICKS_PER_DEGREE;
 
 public class TurretTest {
     Turret turret;
+    TurnLocalization turnLocalization;
     UnitModel unitModel;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         turret = new Turret();
+        turnLocalization = new TurnLocalization(turret, RobotContainer.drivetrain);
         unitModel = new UnitModel(TICKS_PER_DEGREE);
     }
 
@@ -25,7 +31,7 @@ public class TurretTest {
         double maxPos = 200;
         Assert.assertEquals(turret.getNearestTurretPosition(targetAngle, currentPosition, minPos, maxPos), -80, 0.1);
         Assert.assertEquals(turret.getNearestTurretPosition(-359, 200, minPos, maxPos), 1, 0.1);
-        Assert.assertEquals(turret.getNearestTurretPosition(300, -40, minPos, maxPos),-60, 0.1);
+        Assert.assertEquals(turret.getNearestTurretPosition(300, -40, minPos, maxPos), -60, 0.1);
         Assert.assertEquals(turret.getNearestTurretPosition(20, 320, minPos, maxPos), 20, 0.1);
         Assert.assertEquals(turret.getNearestTurretPosition(320, 20, minPos, maxPos), -40, 0.1);
         Assert.assertEquals(turret.getNearestTurretPosition(173, -247, minPos, maxPos), -187, 0.1);
@@ -40,7 +46,15 @@ public class TurretTest {
         Assert.assertEquals(turret.center(currentPos, minPos, maxPos), -160, 0.1);
         Assert.assertEquals(turret.center(-20, minPos, maxPos), -20, 0.1);
         Assert.assertEquals(turret.center(-180, minPos, maxPos), -180, 0.1);
-        Assert.assertEquals(turret.center(-200, minPos, maxPos),160, 0.1);
-        Assert.assertEquals(turret.center(50, minPos, maxPos),50, 0.1);
+        Assert.assertEquals(turret.center(-200, minPos, maxPos), 160, 0.1);
+        Assert.assertEquals(turret.center(50, minPos, maxPos), 50, 0.1);
+    }
+
+    @Test
+    public void TurretLocalization() {
+        Assert.assertEquals(turnLocalization.calculateTargetAngle(new Pose2d(7, 4, new Rotation2d(Math.toRadians(56.4)))), -29.83, 0.1);
+        Assert.assertEquals(turnLocalization.calculateTargetAngle(new Pose2d(3.5, 0.2, new Rotation2d(Math.toRadians(29.4)))), -24.5, 0.1);
+        Assert.assertEquals(turnLocalization.calculateTargetAngle(new Pose2d(3, 0, new Rotation2d(Math.toRadians(20.7)))), -11.23, 0.1);
+        Assert.assertEquals(turnLocalization.calculateTargetAngle(new Pose2d(3.5, 0.2, new Rotation2d(Math.toRadians(-20)))), 24.89, 0.1);
     }
 }
