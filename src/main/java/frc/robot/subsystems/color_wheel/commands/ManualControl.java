eb75a4027538d2e2793b8636921bbe428a13b9b1
oreld <orel.david1502@gmail.com>
2020-02-01 16:38:26 +0200

    Document the code

--- src/main/java/frc/robot/subsystems/color_wheel/commands/ManualControl.java
+++ src/main/java/frc/robot/subsystems/color_wheel/commands/ManualControl.java
@@ -8,8 +8,8 @@ import java.util.function.Supplier;
 
 
 /**
- * Rotates the control panel somewhere around 4 spins, does not mind an overshoot or an undershoot
- * stops at 4 spins
+ * Rotates the control panel manually.
+ * This command would run while you hold a button
  */
 public class ManualControl extends CommandBase {
     private ColorWheel colorWheel;
