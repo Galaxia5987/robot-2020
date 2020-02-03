f84051795d71fa4e09d611231baa5b93dfb2b394
Adam <adam.vichik@gmail.com>
2020-02-03 17:17:56 +0200

    Aim for the inner port

--- src/main/java/frc/robot/subsystems/turret/commands/TurnLocalization.java
+++ src/main/java/frc/robot/subsystems/turret/commands/TurnLocalization.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.turret.Turret;
 
-import static frc.robot.Constants.FieldGeometry.OUTER_POWER_PORT_LOCATION;
+import static frc.robot.Constants.FieldGeometry.INNER_POWER_PORT_LOCATION;
 
 public class TurnLocalization extends CommandBase {
     private final Turret turret;
@@ -23,8 +23,8 @@ public class TurnLocalization extends CommandBase {
     }
 
     public double calculateTargetAngle(Pose2d currentPosition) {
-        double deltaY = OUTER_POWER_PORT_LOCATION.getTranslation().getY() - currentPosition.getTranslation().getY();
-        double deltaX = OUTER_POWER_PORT_LOCATION.getTranslation().getX() - currentPosition.getTranslation().getX();
+        double deltaY = INNER_POWER_PORT_LOCATION.getTranslation().getY() - currentPosition.getTranslation().getY();
+        double deltaX = INNER_POWER_PORT_LOCATION.getTranslation().getX() - currentPosition.getTranslation().getX();
         return Math.toDegrees(Math.atan2(deltaY, deltaX) - currentPosition.getRotation().getRadians());
     }
 }
