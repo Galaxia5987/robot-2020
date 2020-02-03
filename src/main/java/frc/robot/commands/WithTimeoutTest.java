5bf97292bd4d03f05d64d0fd3083fcebac4f850d
Adam <adam.vichik@gmail.com>
2020-02-03 18:22:12 +0200

    Create timer in withTimeoutTest

--- src/main/java/frc/robot/commands/WithTimeoutTest.java
+++ src/main/java/frc/robot/commands/WithTimeoutTest.java
@@ -1,16 +1,20 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Test;
 
 public class WithTimeoutTest extends CommandBase {
     private Test test = new Test();
+    private Timer timer = new Timer();
 
     public WithTimeoutTest() {
     }
 
     @Override
     public void initialize() {
+        timer.reset();
+        timer.start();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -22,12 +26,14 @@ public class WithTimeoutTest extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        System.out.println("With timeout test ended after " + timer.get());
+        timer.stop();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 }
 
