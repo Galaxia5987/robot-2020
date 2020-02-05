ba3543f25c603dcb78ade216769083558e72065f
Paulo Khayat <paulokhayat@gmail.com>
2020-02-05 19:24:51 +0200

    Partial Revert "Revert "Assign commands...""

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -5,8 +5,8 @@ public class Ports {
     public class climber {
         public static final int LEFT_MOTOR = 61;
         public static final int RIGHT_MOTOR = 62;
-        public static final int STOPPER_FORWARD = 1;
-        public static final int STOPPER_REVERSE = 2;
+        public static final int STOPPER_FORWARD = 3;
+        public static final int STOPPER_REVERSE = 0;
         public static final boolean LEFT_MOTOR_INVERTED = false;
         public static final boolean RIGHT_MOTOR_INVERTED = false;
         public static final boolean LEFT_ENCODER_INVERTED = false;
@@ -48,10 +48,9 @@ public class Ports {
         public static final int LEFT_SLAVE = 11;
         public static final int RIGHT_MASTER = 12;
         public static final int RIGHT_SLAVE = 13;
-
-        public static final int SHIFTER_FORWARD_PORT = 0;
-        public static final int SHIFTER_REVERSE_PORT = 0;
-        public static final int SHIFTER_PORT = 0;
+        public static final int SHIFTER_FORWARD_PORT = 6;
+        public static final int SHIFTER_REVERSE_PORT = 5;
+        public static final int SHIFTER_PORT = 5;
     }
 
     public static class ColorWheel {
