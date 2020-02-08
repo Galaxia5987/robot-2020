9fa73243e23daede8b5e620c0ae3d00f4ea8bc6c
lior <lior5858@gmail.com>
2020-02-08 19:06:00 +0200

    Undo rapid robot container changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,6 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.climb.Climber;
 import frc.robot.subsystems.color_wheel.ColorWheel;
 import frc.robot.subsystems.color_wheel.commands.ManualControl;
@@ -20,7 +19,6 @@ import frc.robot.subsystems.color_wheel.commands.RotationControl;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.conveyor.commands.FeedTurret;
 import frc.robot.subsystems.drivetrain.Drivetrain;
-import frc.robot.subsystems.drivetrain.commands.GearShift;
 import frc.robot.subsystems.drivetrain.commands.JoystickDrive;
 import frc.robot.subsystems.intake.Intake;
 import frc.robot.subsystems.intake.commands.IntakeBalls;
@@ -29,8 +27,6 @@ import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.subsystems.shooter.commands.SpeedUp;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.commands.JoystickTurret;
-import frc.robot.subsystems.turret.commands.TurnTurret;
-import frc.robot.utilities.State;
 import frc.robot.utilities.StickButton;
 import frc.robot.valuetuner.ValueTuner;
 import org.techfire225.webapp.Webserver;
@@ -51,7 +47,7 @@ public class RobotContainer {
     private final ColorWheel colorWheel = new ColorWheel();
     private final Shooter shooter = new Shooter();
     private final Intake intake = new Intake();
-    private  Conveyor conveyor = new Conveyor(intake);
+    private final Conveyor conveyor = new Conveyor(intake);
     public static final Climber climber = new Climber();
     public static final Turret turret = new Turret();
     private final Command m_autoCommand = null;
@@ -64,7 +60,6 @@ public class RobotContainer {
         configureButtonBindings();
         if (Robot.debug) {
             startFireLog();
-            startValueTuner();
         }
     }
 
@@ -72,35 +67,26 @@ public class RobotContainer {
      * Defines the default command of each mechanism on the robot.
      */
     private void configureDefaultCommands(){
-//        colorWheel.setDefaultCommand(new ManualControl(colorWheel));
-//        turret.setDefaultCommand(new JoystickTurret(turret));
-//        drivetrain.setDefaultCommand(new JoystickDrive(drivetrain));
+        colorWheel.setDefaultCommand(new ManualControl(colorWheel));
+        turret.setDefaultCommand(new JoystickTurret(turret));
+        drivetrain.setDefaultCommand(new JoystickDrive(drivetrain));
     }
     /**
      * Configures all of the button usages on the robot.
      */
     private void configureButtonBindings() {
-//        OI.a.whenPressed(new TurnTurret(turret, 0));
-//        OI.x.whenPressed(new TurnTurret(turret, 90));
-//        OI.b.whenPressed(new TurnTurret(turret, 180));
-//        OI.y.whenPressed(new TurnTurret(turret, 27));
-//        OI.x.whenPressed(new InstantCommand(() -> intake.setPosition(State.TOGGLE)));
-        OI.b.whileHeld(new IntakeBalls(intake, 0.35));
-        OI.a.whileHeld(new OuttakeBalls(conveyor, intake, 0.2, 0.5));
-        OI.select.whileHeld(new FeedTurret(conveyor));
-//
-//        OI.a.whenPressed(new InstantCommand(() -> conveyor.openGate(true)));//0
-//        OI.y.whenPressed(new InstantCommand(() -> conveyor.openGate(false)));//1
-
-//        OI.select.whenPressed(new Instan
-//        tCommand(CommandScheduler.getInstance()::cancelAll));
-//        OI.rb.whenPressed(new RotationControl(colorWheel));
-//        OI.lb.whenPressed(new PositionControl(colorWheel));
-//        OI.select_start.whenHeld(new SequentialCommandGroup(
-//                new WaitCommand(2),
-//                new RunCommand(() -> Robot.shootingManualMode = true)
-//        )); //If both buttons are held without being released the manualMode will be enabled.
-//        OI.start.whenPressed(() -> Robot.shootingManualMode = false); //Pressing start disables the manual mode for shooting.
+        OI.a.whenPressed(new IntakeBalls(intake, 0.35));
+        OI.x.whileHeld(new OuttakeBalls(conveyor, intake, 0.2, 0.5));
+        OI.b.whenPressed(new SpeedUp(shooter));
+        OI.y.whenPressed(new FeedTurret(conveyor));
+        OI.select.whenPressed(new InstantCommand(CommandScheduler.getInstance()::cancelAll));
+        OI.rb.whenPressed(new RotationControl(colorWheel));
+        OI.lb.whenPressed(new PositionControl(colorWheel));
+        OI.select_start.whenHeld(new SequentialCommandGroup(
+                new WaitCommand(2),
+                new RunCommand(() -> Robot.shootingManualMode = true)
+        )); //If both buttons are held without being released the manualMode will be enabled.
+        OI.start.whenPressed(() -> Robot.shootingManualMode = false); //Pressing start disables the manual mode for shooting.
     }
 
     /**
@@ -129,5 +115,5 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         return null;
     }
-      
+
 }
