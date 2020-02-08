691d9ce7710c39a136f57cd1cc2be8f7cb55af0d
lior <lior5858@gmail.com>
2020-02-08 18:17:24 +0200

    Correct shooter inversion

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -30,7 +30,7 @@ public class Ports {
         public static final int SLAVE_1 = 24;
         public static final int SLAVE_2 = 25;
         public static final boolean IS_MASTER_INVERTED = true;
-        public static final boolean IS_SLAVE_1_INVERTED = true;
+        public static final boolean IS_SLAVE_1_INVERTED = false;
         public static final boolean IS_SLAVE_2_INVERTED = false;
         public static final boolean IS_ENCODER_INVERTED = false;
     }
