d5c6a011c5e8005b12f75d20440b5374187a1d07
lior <lior5858@gmail.com>
2020-02-08 18:18:28 +0200

    Initialize compressor and pdp

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,10 +8,7 @@
 package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -29,7 +26,8 @@ public class Robot extends TimedRobot {
     public static boolean isRobotA = !new DigitalInput(0).get();
     public static boolean hasShifter = true;
     public static boolean shootingManualMode = false;
-
+    public static Compressor compressor = new Compressor();
+    public static PowerDistributionPanel pdp = new PowerDistributionPanel();
     private Command m_autonomousCommand;
     public static Timer robotTimer = new Timer();
     private RobotContainer m_robotContainer;
@@ -96,6 +94,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
+        compressor.stop();
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
