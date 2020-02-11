adf23200edaeb13809106a2e5f880045799d5395
Paulo Khayat <paulokhayat@gmail.com>
2020-02-11 10:51:42 +0200

    Add intaking code for funnel

--- src/main/java/frc/robot/subsystems/conveyor/commands/LoadConveyor.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/LoadConveyor.java
@@ -20,7 +20,8 @@ public class LoadConveyor extends CommandBase {
     @Override
     public void initialize() {
         conveyor.setGate(State.CLOSE);
-        conveyor.setPower(CONVEYOR_MOTOR_INTAKE_POWER.get());
+        conveyor.setConveyorPower(CONVEYOR_MOTOR_FEED_POWER);
+        conveyor.setFunnelPower(FUNNEL_MOTOR_FEED_POWER.get());
     }
 
     @Override
