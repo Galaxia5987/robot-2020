29d3315daa0b9e7da8081b1af9131f15639f53e5
oreld <orel.david1502@gmail.com>
2020-02-12 12:34:25 +0200

    Change the gearShift command to instant command

--- src/main/java/frc/robot/subsystems/drivetrain/commands/GearShift.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/GearShift.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems.drivetrain.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -16,41 +17,34 @@ import static frc.robot.Constants.Drivetrain.*;
 /**
  *
  */
-public class GearShift extends CommandBase {
-  private Drivetrain.shiftModes shiftmode;
-  private final Drivetrain drivetrain;
+public class GearShift extends InstantCommand {
+    private Drivetrain.shiftModes shiftmode;
+    private final Drivetrain drivetrain;
 
-  /**
-   *
-   *
-   *
-   */
-  public GearShift(Drivetrain drivetrain, Drivetrain.shiftModes shiftmode) {
-    this.shiftmode = shiftmode;
-    this.drivetrain = drivetrain;
-  }
+    /**
+     *
+     */
+    public GearShift(Drivetrain drivetrain, Drivetrain.shiftModes shiftmode) {
+        this.shiftmode = shiftmode;
+        this.drivetrain = drivetrain;
+    }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    drivetrain.shiftGear(shiftmode);
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        drivetrain.shiftGear(shiftmode);
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
 
-  }
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    drivetrain.resetCooldown();
-  }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.resetCooldown();
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return drivetrain.getCooldown() > SHIFTER_COOLDOWN;
-  }
 }
