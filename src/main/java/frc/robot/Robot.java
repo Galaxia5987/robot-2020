e8486e95b6fcc91c0fda3a2ef3831e5cbe4435aa
Repulser <thetechproject2014@gmail.com>
2020-02-12 18:43:27 +0200

    Add PCM port to compressor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -25,7 +25,7 @@ public class Robot extends TimedRobot {
     // The roboRIO has built-in pull up resistors, bridge signal and ground pins on Robot A DIO 0.
     public static boolean isRobotA = !new DigitalInput(0).get();
     public static boolean shootingManualMode = false;
-    public static Compressor compressor = new Compressor();
+    public static Compressor compressor = new Compressor(Ports.PCM);
     public static PowerDistributionPanel pdp = new PowerDistributionPanel();
     private Command m_autonomousCommand;
     public static Timer robotTimer = new Timer();
