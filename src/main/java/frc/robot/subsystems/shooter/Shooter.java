ab30f3309381d5e4d2c21c0b81a97eb1512255d1
Repulser <thetechproject2014@gmail.com>
2020-02-13 14:18:45 +0200

    Improve shooter config

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems.shooter;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.networktables.NetworkTable;
@@ -13,6 +11,7 @@ import frc.robot.UtilityFunctions;
 import frc.robot.subsystems.UnitModel;
 import frc.robot.utilities.VictorConfiguration;
 import frc.robot.valuetuner.WebConstantPIDTalon;
+import org.techfire225.webapp.FireLog;
 
 import static frc.robot.Constants.Shooter.*;
 import static frc.robot.Constants.TALON_TIMEOUT;
@@ -35,12 +34,6 @@ public class Shooter extends SubsystemBase {
 
         VictorConfiguration slaveConfigs = new VictorConfiguration();
 
-        // Basic motor configurations
-        // Master configurations
-        shooterMaster.configFactoryDefault();
-        shooterSlave1.configFactoryDefault();
-        shooterSlave2.configFactoryDefault();
-
         shooterMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, TALON_TIMEOUT);
         shooterMaster.setInverted(IS_MASTER_INVERTED);
         shooterMaster.setSensorPhase(IS_ENCODER_INVERTED);
@@ -51,14 +44,8 @@ public class Shooter extends SubsystemBase {
         shooterMaster.config_kD(TALON_PID_SLOT, KD, TALON_TIMEOUT);
         shooterMaster.config_kF(TALON_PID_SLOT, KF, TALON_TIMEOUT);
 
-        // Electrical (master)
-        shooterMaster.configVoltageCompSaturation(12);
-        shooterMaster.enableVoltageCompensation(true);
-        shooterMaster.configPeakCurrentLimit(MAX_CURRENT);
-
-        shooterMaster.configPeakCurrentLimit(0);
-        shooterMaster.configContinuousCurrentLimit(40);
-        shooterMaster.enableCurrentLimit(true);
+        shooterMaster.configForwardLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.Disabled);
+        shooterMaster.configReverseLimitSwitchSource(LimitSwitchSource.Deactivated, LimitSwitchNormal.Disabled);
 
         // Slave configuration
         shooterSlave1.follow(shooterMaster);
@@ -66,11 +53,6 @@ public class Shooter extends SubsystemBase {
         shooterSlave1.setInverted(IS_SLAVE_1_INVERTED);
         shooterSlave2.setInverted(IS_SLAVE_2_INVERTED);
 
-        shooterMaster.setNeutralMode(NeutralMode.Coast);
-
-        slaveConfigs.setNeutralMode(NeutralMode.Coast);
-        slaveConfigs.setEnableVoltageCompensation(true);
-        slaveConfigs.setVoltageCompensationSaturation(12);
         UtilityFunctions.configAllVictors(slaveConfigs, shooterSlave1, shooterSlave2);
         new WebConstantPIDTalon("shooterTalon", KP, KI, KD, KF, shooterMaster);
     }
