f5ffc4e4b0ec03708e7d7300cffc1c772d4aaf22
Repulser <thetechproject2014@gmail.com>
2020-02-13 16:49:30 +0200

    Add methods to get the pose according to the vision
    and the normalized distance from the port (the component of the distance on the X axis)

--- src/main/java/frc/robot/utilities/VisionModule.java
+++ src/main/java/frc/robot/utilities/VisionModule.java
@@ -3,10 +3,14 @@ package frc.robot.utilities;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import frc.robot.Constants;
 
 public class VisionModule {
     private static NetworkTable visionTable = NetworkTableInstance.getDefault().getTable("chameleon-vision").getSubTable("ps3");
     private static NetworkTableEntry visionAngle = visionTable.getEntry("targetYaw");
+    private static NetworkTableEntry visionPose = visionTable.getEntry("targetPose");
     private static NetworkTableEntry visionValid = visionTable.getEntry("isValid");
     private static NetworkTableEntry leds = visionTable.getEntry("leds");
 
@@ -26,6 +30,15 @@ public class VisionModule {
 
     public static void setLEDs(boolean on) {
         leds.setBoolean(on);
+    }
+
+    public static Pose2d getPose() {
+        Double[] pose = visionPose.getDoubleArray(new Double[]{});
+        return new Pose2d(pose[0], pose[1], new Rotation2d(pose[2]));
+    }
+
+    public static double getNormalizeDistance(){
+        return Math.pow(Constants.FieldGeometry.PORT_HEIGHT, 2) - Math.pow(Constants.VISION_MODULE_HEIGHT, 2);
     }
 
 }
