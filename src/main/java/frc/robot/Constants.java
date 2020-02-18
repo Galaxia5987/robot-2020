934ac087e7d6e1a0f8cab9c35f5875ff93c9b296
Repulser <thetechproject2014@gmail.com>
2020-02-18 21:56:52 +0200

    Move constant swapping around

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,12 +2,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import frc.robot.utilities.Utils;
 import frc.robot.valuetuner.WebConstant;
 import org.apache.commons.lang.math.DoubleRange;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Optional;
+
+import static org.apache.commons.lang3.ObjectUtils.CONST;
 
 /**
  * A class holding all of the constants of every mechanism on the robot.
@@ -199,46 +202,6 @@ public class Constants {
         public static final WebConstant MODIFY_JOYSTICK_RATE = new WebConstant("climbJoystickRate", 0.7); // The factor which the value of the joystick is multiplied by to calculate the change rate.
         public static final double MAX_DIFFERENCE = 2; // The maximal difference between the two sides of the climber.
         public static final double DISTANCE_BETWEEN_RODS = 0; // The distance between both climbing rods.
-    }
-
-    static { // Runs alongside main
-        if (!Robot.isRobotA) { // We want robot B constants
-            replaceFields(Constants.class, BConstants.class); // Replace outer constants
-            for (Class aClass : Constants.class.getDeclaredClasses()) { // Loop constants classes
-                // Find the class in B Constants
-                Optional<Class<?>> bClass = Arrays.stream(BConstants.class.getDeclaredClasses()).filter(c -> c.getSimpleName().equals(aClass.getSimpleName())).findAny();
-                if (bClass.isEmpty()) continue; // Class isn't present
-                replaceFields(aClass, bClass.get());
-            }
-        }
-    }
-
-    /**
-     * Replaces fields between constants classes.
-     *
-     * @param class1 Original constants class
-     */
-    public static void replaceFields(Class class1, Class class2) {
-        //Loop and replace all fields
-        for (Field f : class2.getDeclaredFields()) {
-            for (Field f2 : class1.getDeclaredFields()) {
-                //Loop and replace all fields
-                if (f2.getName().equals(f.getName())) { // If the name is equal perform replacement
-
-                    f2.setAccessible(true);
-                    f.setAccessible(true);
-                    try {
-                        Field modifiersField = Field.class.getDeclaredField("modifiers");
-                        modifiersField.setAccessible(true);
-                        modifiersField.setInt(f2, f2.getModifiers() & ~Modifier.FINAL);
-                        f2.set(null, f.get(null));
-                    } catch (IllegalAccessException | NoSuchFieldException e) { // Catch relevant exceptions
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
     }
 }
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utilities.Utils;
 
 
 /**
@@ -44,6 +45,10 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void robotInit() {
+        if(!Robot.isRobotA) {
+            Utils.swapConstants(Constants.class, BConstants.class);
+            Utils.swapConstants(Ports.class, BPorts.class);
+        }
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotTimer.reset();
