b9c90521c42507f1cdfdfbd136419539dbff5e2e
Barel Shlidor <barel@shlidor.org>
2020-02-18 13:52:52 +0200

    Add timer to feed turret command

--- src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
@@ -1,32 +1,31 @@
 package frc.robot.subsystems.conveyor.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.utilities.State;
-import frc.robot.subsystems.shooter.Shooter;
-import frc.robot.subsystems.turret.Turret;
 
 import java.util.function.Supplier;
 
 import static frc.robot.Constants.Conveyor.*;
-import static frc.robot.Constants.Shooter.VELOCITY_TOLERANCE;
 
 /**
  * Open the mechanical stopper and feed Power Cells into the shooter.
  */
 public class FeedTurret extends CommandBase {
     private Conveyor conveyor;
+    private Timer timer = new Timer();
     private Supplier<Boolean> isShooterReady;
     private Supplier<Boolean> isTurretReady;
     private Supplier<Boolean> isShooting;
-    private boolean smartFeed = false; //In cases where we want to feed at a constant rate. TODO: Deprecate this once we are confident with constant velocity checking.
+    private boolean smartFeed; //In cases where we want to feed at a constant rate. TODO: Deprecate this once we are confident with constant velocity checking.
 
     public FeedTurret(Conveyor conveyor) {
         this(conveyor, () -> true, () -> true, () -> true);
         smartFeed = false;
     }
 
-    public FeedTurret(Conveyor conveyor, Supplier<Boolean> isShooterReady, Supplier<Boolean> isTurretReady, Supplier<Boolean> isShooting){
+    public FeedTurret(Conveyor conveyor, Supplier<Boolean> isShooterReady, Supplier<Boolean> isTurretReady, Supplier<Boolean> isShooting) {
         addRequirements(conveyor);
         this.conveyor = conveyor;
         this.isShooterReady = isShooterReady;
@@ -38,27 +37,26 @@ public class FeedTurret extends CommandBase {
 
     @Override
     public void initialize() {
+        timer.reset();
+        timer.start();
     }
 
     @Override
     public void execute() {
         if (smartFeed && isShooting.get()) {
-            if (isShooterReady.get() && isTurretReady.get()) {
+            if (isShooterReady.get() && isTurretReady.get() && timer.get() >= GATE_OPEN_TIME) {
                 if (conveyor.isGateOpen()) {
                     conveyor.setConveyorPower(CONVEYOR_MOTOR_OPEN_FEED_POWER.get());
                     conveyor.setFunnelPower(FUNNEL_MOTOR_FEED_POWER.get());
-                }
-                else
+                } else
                     conveyor.setGate(State.OPEN);
 
-            }
-            else {
+            } else {
                 conveyor.setConveyorPower(0);
                 conveyor.setFunnelPower(0);
                 conveyor.setGate(State.CLOSE);
             }
-        }
-        else {
+        } else {
             conveyor.setConveyorPower(CONVEYOR_MOTOR_OPEN_FEED_POWER.get());
             conveyor.setFunnelPower(FUNNEL_MOTOR_FEED_POWER.get());
         }
@@ -71,7 +69,8 @@ public class FeedTurret extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
-        conveyor.stop();
+        timer.stop();
+        conveyor.stopAll();
         conveyor.setGate(State.CLOSE);
     }
 }
