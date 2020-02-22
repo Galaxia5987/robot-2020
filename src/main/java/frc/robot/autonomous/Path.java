1330f03fae30c3b5c6010adadef38d0d2422a7cd
Repulser <thetechproject2014@gmail.com>
2020-02-22 19:11:45 +0200

    Add turn constraint

--- src/main/java/frc/robot/autonomous/Path.java
+++ src/main/java/frc/robot/autonomous/Path.java
@@ -4,19 +4,23 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
 import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.constraint.CentripetalAccelerationConstraint;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static frc.robot.Constants.Autonomous.MAX_ACCELERATION;
-import static frc.robot.Constants.Autonomous.MAX_SPEED;
+import static frc.robot.Constants.Autonomous.*;
 
 public class Path {
     private static final TrajectoryConfig DEFAULT_CONFIG = new TrajectoryConfig(MAX_SPEED, MAX_ACCELERATION);
     private TrajectoryConfig config = DEFAULT_CONFIG;
     private List<Pose2d> waypoints;
     private Trajectory trajectory;
+
+    static {
+        DEFAULT_CONFIG.addConstraint(new CentripetalAccelerationConstraint(MAX_CENTRIPETAL_ACCELERATION));
+    }
 
     Path(Pose2d... points) {
         waypoints = new ArrayList<>(Arrays.asList(points));
