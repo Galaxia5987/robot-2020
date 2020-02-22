1902ca74a18f4c32662a2ed821811e5fba02ba5b
Repulser <thetechproject2014@gmail.com>
2020-02-23 00:57:04 +0200

    Use motion magic for big motions

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -111,13 +111,13 @@ public class Turret extends SubsystemBase {
     public void setAngle(double angle) {
         targetAngle = normalizeSetpoint(angle);
         //Use motion magic if target angle is big enough, else use tracking PID.
-//        if (Math.abs(targetAngle - getAngle()) < CONTROL_MODE_THRESHOLD) {
+        if (Math.abs(targetAngle - getAngle()) < CONTROL_MODE_THRESHOLD) {
             setPidSlot(POSITION_PID_SLOT);
             motor.set(ControlMode.Position, unitModel.toTicks(targetAngle)); // Set the position to the target angle plus the backlash the turret creates.
-//        } else {
-//            setPidSlot(MOTION_MAGIC_PID_SLOT);
-//            motor.set(ControlMode.MotionMagic, unitModel.toTicks(targetAngle));
-//        }
+        } else {
+            setPidSlot(MOTION_MAGIC_PID_SLOT);
+            motor.set(ControlMode.MotionMagic, unitModel.toTicks(targetAngle));
+        }
 
     }
 
