f761cd07a061d3d78bd17597819f1cf54503a35a
Repulser <thetechproject2014@gmail.com>
2020-02-23 19:44:12 +0200

    remove spaces

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -91,7 +91,7 @@ public class Constants {
         public static final WebConstant CONVEYOR_MOTOR_INTAKE_POWER = new WebConstant("conveyorIntakePower", 0.7);
         public static final WebConstant CONVEYOR_OUTTAKE_POWER = new WebConstant("conveyorOuttakePower", 0.5);
         public static final WebConstant FUNNEL_OUTTAKE_POWER = new WebConstant("funnelOuttakePower", 0.5);
-        public static final WebConstant FEED_OUTTAKE_POWER = new WebConstant("feed outtake power", 0.6);
+        public static final WebConstant FEED_OUTTAKE_POWER = new WebConstant("feedOuttakePower", 0.6);
 
         public static final double CONVEYOR_MOTOR_RETURN_POWER = 0;
         public static final double FEED_TIMEOUT = 5;
