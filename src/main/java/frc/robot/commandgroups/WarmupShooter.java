14d1fc4a788edf32e75ce0e6047dadc607f90983
lior <lior5858@gmail.com>
2020-02-24 15:44:05 +0200

    Create WarmupShooter commandGroup

---
+++ src/main/java/frc/robot/commandgroups/WarmupShooter.java
@@ -0,0 +1,17 @@
+package frc.robot.commandgroups;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.commands.SpeedUp;
+import frc.robot.subsystems.turret.Turret;
+import frc.robot.subsystems.turret.commands.TurretSwitching;
+
+public class WarmupShooter extends ParallelCommandGroup {
+    public WarmupShooter(Turret turret, Shooter shooter, Drivetrain drivetrain){
+        addCommands(new TurretSwitching(turret, drivetrain));
+        addCommands(new SpeedUp(shooter, drivetrain));
+    }
+}
