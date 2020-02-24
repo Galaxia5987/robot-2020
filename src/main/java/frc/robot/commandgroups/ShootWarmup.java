52f49f295a18170e835446296fc53fd3d7bbec92
lior <lior5858@gmail.com>
2020-02-24 15:54:29 +0200

    Rename WarmupShooter commandgroup

--- src/main/java/frc/robot/commandgroups/ShootWarmup.java
+++ src/main/java/frc/robot/commandgroups/ShootWarmup.java
@@ -7,9 +7,9 @@ import frc.robot.subsystems.shooter.commands.SpeedUp;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.commands.TurretSwitching;
 
-public class WarmupShooter extends ParallelCommandGroup {
+public class ShootWarmup extends ParallelCommandGroup {
 
-    public WarmupShooter(Turret turret, Shooter shooter, Drivetrain drivetrain) {
+    public ShootWarmup(Turret turret, Shooter shooter, Drivetrain drivetrain) {
         addCommands(new TurretSwitching(turret, drivetrain));
         addCommands(new SpeedUp(shooter, drivetrain));
     }
