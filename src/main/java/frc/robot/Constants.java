bb738b1636d35cdf5e8c7df679cdb702b640d435
Repulser <thetechproject2014@gmail.com>
2020-02-25 15:39:52 +0200

    Make dead zone angles bigger

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -117,7 +117,7 @@ public class Constants {
         public static final double TICKS_PER_DEGREE = CONST(TICKS_PER_ROTATION / 360.0);
 
         public static final DoubleRange ALLOWED_ANGLES = new DoubleRange(-42, 264);
-        public static final DoubleRange DEAD_ZONE_ANGLES = new DoubleRange(41, 83);
+        public static final DoubleRange DEAD_ZONE_ANGLES = new DoubleRange(38, 86);
 
         public static final double UNREACHABLE_ANGLE = 300; //This is an angle which the turret can't mechanically pass. If the turret passes this angle from either direction before startup, the turret will malfunction.
         public static final int ZERO_POSITION = CONST(1600); //Encoder absolute position when the turret is facing forward. This might change occasionally.
