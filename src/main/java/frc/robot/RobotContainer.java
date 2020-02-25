a05fb080b3aeb2b718e0062e73ab125c4f327a31
Paulo Khayat <paulokhayat@gmail.com>
2020-02-26 00:39:40 +0200

    Make LED static

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -61,7 +61,7 @@ public class RobotContainer {
     private final Conveyor conveyor = new Conveyor(intake);
     public static final Climber climber = new Climber();
     public static final Turret turret = new Turret();
-    private final LED led = new LED(STRIP_LENGTH);
+    private static final LED led = new LED(STRIP_LENGTH);
     private final Command m_autoCommand = null;
 
     /**
