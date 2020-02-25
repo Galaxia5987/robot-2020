c4443c8a8d57d0eda029a9d45f6c6c284464abf0
Repulser <thetechproject2014@gmail.com>
2020-02-25 15:58:51 +0200

    Fix shooter inversion

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -86,9 +86,9 @@ public class Ports {
 
 class BPorts{
     public static class Shooter {
-        public static final boolean IS_MASTER_INVERTED = true;
+        public static final boolean IS_MASTER_INVERTED = false;
         public static final boolean IS_SLAVE_1_INVERTED = true;
-        public static final boolean IS_SLAVE_2_INVERTED = false;
+        public static final boolean IS_SLAVE_2_INVERTED = true;
         public static final boolean IS_ENCODER_INVERTED = false;
     }
 
