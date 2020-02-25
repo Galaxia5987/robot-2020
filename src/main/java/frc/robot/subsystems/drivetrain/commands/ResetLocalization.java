f3deb76e88437c4272442754ad3c254c0dc70576
Paulo Khayat <paulokhayat@gmail.com>
2020-02-25 14:56:42 +0200

    Create empty reset localization

---
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/ResetLocalization.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.drivetrain.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.turret.Turret;
+
+public class ResetLocalization extends CommandBase {
+
+    public ResetLocalization(Drivetrain drivetrain, Turret turret){
+
+    }
+
+}
