2576909e5c432b88f1e6a23999bb097001d5139f
Repulser <thetechproject2014@gmail.com>
2020-02-25 18:12:08 +0200

    Improve logic

--- src/main/java/frc/robot/subsystems/drivetrain/commands/ResetLocalization.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/ResetLocalization.java
@@ -1,14 +1,36 @@
 package frc.robot.subsystems.drivetrain.commands;
 
+import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.utilities.VisionModule;
 
 public class ResetLocalization extends CommandBase {
+    private final Drivetrain drivetrain;
+    private Pose2d pose;
 
     public ResetLocalization(Drivetrain drivetrain) {
-        drivetrain.setPose(VisionModule.getRobotPose());
+        this.drivetrain = drivetrain;
     }
 
+    @Override
+    public void initialize() {
+        pose = null;
+    }
+
+    @Override
+    public void execute() {
+        pose = VisionModule.getRobotPose();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pose != null;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrain.setPose(pose);
+    }
 }
