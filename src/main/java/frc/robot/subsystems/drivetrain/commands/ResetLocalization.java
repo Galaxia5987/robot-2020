f669645725f6488df227a94ac41dba4a07ec8da9
Repulser <thetechproject2014@gmail.com>
2020-02-25 18:09:30 +0200

    Reset localization using vision

--- src/main/java/frc/robot/subsystems/drivetrain/commands/ResetLocalization.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/ResetLocalization.java
@@ -3,11 +3,12 @@ package frc.robot.subsystems.drivetrain.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.turret.Turret;
+import frc.robot.utilities.VisionModule;
 
 public class ResetLocalization extends CommandBase {
 
-    public ResetLocalization(Drivetrain drivetrain, Turret turret){
-
+    public ResetLocalization(Drivetrain drivetrain) {
+        drivetrain.setPose(VisionModule.getRobotPose());
     }
 
 }
