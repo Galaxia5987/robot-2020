f7e5e52fccd399506b0ffb9c5eb7718ba5f116aa
Repulser <thetechproject2014@gmail.com>
2020-02-25 19:10:19 +0200

    Fix ShootAndReset

--- src/main/java/frc/robot/autonomous/ShootAndReset.java
+++ src/main/java/frc/robot/autonomous/ShootAndReset.java
@@ -1,22 +1,21 @@
 package frc.robot.autonomous;
 
-import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.commandgroups.AutoShoot;
 import frc.robot.subsystems.conveyor.Conveyor;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.shooter.commands.SpeedUp;
 import frc.robot.subsystems.turret.Turret;
 import frc.robot.subsystems.turret.commands.VisionTurret;
-import frc.robot.utilities.CustomDashboard;
 import frc.robot.utilities.VisionModule;
 
 import java.util.List;
 
-public class ShootAndReset extends ParallelCommandGroup {
+public class ShootAndReset extends SequentialCommandGroup {
 
     public ShootAndReset(Turret turret, Shooter shooter, Conveyor conveyor, Drivetrain drivetrain, List<Path> toGenerate, double shootTime) {
-        addCommands(new AutoDelay());
+        addCommands(new ParallelDeadlineGroup(new AutoDelay(), new SpeedUp(shooter, false, drivetrain)));
         addCommands(new ParallelCommandGroup( // Initiate position while shooting balls
                 new SequentialCommandGroup(
                         new ParallelDeadlineGroup(
