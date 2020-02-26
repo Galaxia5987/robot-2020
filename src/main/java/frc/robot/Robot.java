bd34eddffd3c919c4aef6a4fa05c16e3b3f57c94
driver-station <paulokhayat@gmail.com>
2020-02-26 11:43:50 +0200

    Set power to 0 instead of angle

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -185,7 +185,7 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousInit() {
-        turret.setAngle(turret.getAngle());
+        turret.setPower(0);
         m_robotContainer.drivetrain.setBrake(true);
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
         // schedule the autonomous command (example)
