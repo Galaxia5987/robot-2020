99ab7b7f126d868fa2d7919e86ba47b64319865e
driver-station <paulokhayat@gmail.com>
2020-02-27 09:07:53 +0200

    Reset ball count in auto init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -136,6 +136,7 @@ public class Robot extends TimedRobot {
         if (m_autonomousCommand != null) {
             m_autonomousCommand.schedule();
         }
+        m_robotContainer.conveyor.resetBallCount();
     }
 
     /**
