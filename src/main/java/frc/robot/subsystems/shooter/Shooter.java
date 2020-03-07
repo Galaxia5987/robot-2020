49a952de61ac447e6aef5114678ec9bfdfc7c99a
Unknown <orel.david1502@gmail.com>
2020-03-07 14:58:40 +0200

    Change the setAngle method because the servo is reversed

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -125,7 +125,7 @@ public class Shooter extends SubsystemBase {
      * @param angle angle in degrees of the hood
      */
     public void setHoodAngle(double angle){
-        adjustableHood.setAngle(angle);
+        adjustableHood.setAngle(180 - angle);
     }
 
     public void setHoodAngleByDistance(double distance){
