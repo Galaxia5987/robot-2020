9b205cdd378b11cfb51e259831b94b35971d123a
lior <lior5858@gmail.com>
2020-03-08 10:11:27 +0200

    Adjust conveyor feed power

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -266,7 +266,7 @@ class BConstants {
     }
 
     public static class Conveyor {
-        public static final double CONVEYOR_FEED_POWER = 0.6;
+        public static final double CONVEYOR_FEED_POWER = 0.75;
     }
 
     public static class Shooter {
