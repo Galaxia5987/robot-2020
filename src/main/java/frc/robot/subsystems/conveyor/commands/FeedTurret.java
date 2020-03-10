12bb9e3d0273df1fe9c87e64b3b0e35c3b835a54
Unknown <orel.david1502@gmail.com>
2020-03-10 22:46:43 +0200

    Fix null check

--- src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
+++ src/main/java/frc/robot/subsystems/conveyor/commands/FeedTurret.java
@@ -46,7 +46,7 @@ public class FeedTurret extends CommandBase {
 
     public FeedTurret(Conveyor conveyor, Intake intake, Supplier<Boolean> isShooterReady, Supplier<Boolean> isTurretReady, Supplier<Boolean> isShooting, boolean outtake) {
         addRequirements(conveyor);
-        if(intake == null)
+        if(intake != null)
             addRequirements(intake);
         this.conveyor = conveyor;
         this.intake = intake;
