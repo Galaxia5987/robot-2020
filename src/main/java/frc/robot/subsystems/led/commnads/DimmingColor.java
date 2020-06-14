b06bf8abeb3e8c9f1f43d03833f68ad166ffe4d7
Paulo Khayat <paulokhayat@gmail.com>
2020-06-14 20:58:54 +0300

    Add range to dimming color

--- src/main/java/frc/robot/subsystems/led/commnads/DimmingColor.java
+++ src/main/java/frc/robot/subsystems/led/commnads/DimmingColor.java
@@ -12,18 +12,24 @@ import frc.robot.subsystems.led.LEDUtilities;
  */
 
 public class DimmingColor extends GenericTimerLED {
-    private double frequency; //ms
+    private final double frequency; //ms
+    private final double value_diff; //The amount the value changes. by default, it dims from the colors value to 0.
     private double[] colorHSV;
-    public DimmingColor(LED led, Color color, double frequency, double timeout) {
+    public DimmingColor(LED led, Color color, double frequency, double timeout, double value_range) {
         super(led);
         this.timeout = timeout;
         this.frequency = frequency;
         this.delay = 20;
         colorHSV = HSV.Color2hsv(color);
+        this.value_diff = value_range;
+    }
+
+    public DimmingColor(LED led, Color color, double frequency, double timeout){
+        this(led, color, frequency, timeout, HSV.Color2hsv(color)[2]);
     }
 
     @Override
     protected AddressableLEDBuffer tick(double time){
-        return LEDUtilities.singleColor(led.getLength(),HSV.hsv2Color(colorHSV[0],colorHSV[1],colorHSV[2] * (2-Math.cos(2*Math.PI*time/frequency))));
+        return LEDUtilities.singleColor(led.getLength(),HSV.hsv2Color(colorHSV[0],colorHSV[1],(colorHSV[2] - value_diff) + value_diff * (2-Math.cos(2*Math.PI*time/frequency))));
     }
 }
