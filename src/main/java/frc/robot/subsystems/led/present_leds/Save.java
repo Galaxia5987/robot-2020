0cbe008afe858f10f642dd3275bd48cb67128e7a
Paulo Khayat <paulokhayat@gmail.com>
2020-06-15 04:37:29 +0300

    Change test

--- src/main/java/frc/robot/subsystems/led/present_leds/Save.java
+++ src/main/java/frc/robot/subsystems/led/present_leds/Save.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems.led.present_leds;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.subsystems.led.HSV;
 import frc.robot.subsystems.led.LEDUtilities;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import java.awt.*;
 import java.io.*;
+import java.util.Arrays;
+
 class Save
 {
     public static void main(String args[])
@@ -14,11 +17,17 @@ class Save
             // Create file
             FileWriter fstream = new FileWriter("src\\main\\java\\frc\\robot\\subsystems\\led\\present_leds\\" + "strip.txt");
             BufferedWriter out = new BufferedWriter(fstream);
-            AddressableLEDBuffer buffer = LEDUtilities.hsvBlendColors(22, false,
-                    new ImmutablePair<>(3, edu.wpi.first.wpilibj.util.Color.kYellow),
-                    new ImmutablePair<>(10, edu.wpi.first.wpilibj.util.Color.kRed),
-                    new ImmutablePair<>(17, Color.kBlue));
+            AddressableLEDBuffer buffer = LEDUtilities.blendColors(22, true,
+                    new ImmutablePair<>(0, Color.kRed),
+                    new ImmutablePair<>(5, HSV.hsv2Color( 360 * 5 / 22., 1, 1)),
+                    new ImmutablePair<>(11, Color.kCyan),
+                    new ImmutablePair<>(17, HSV.hsv2Color(360*17/22., 1, 1))
+            );
+            LEDUtilities.printBuffer(LEDUtilities.getShifted(LEDUtilities.colorLengths(22,
+                    new ImmutablePair<>(1, Color.kRed),
+                    new ImmutablePair<>(21, Color.kBlack)), 2));
             for (int i = 0; i < buffer.getLength(); i++){
+                System.out.println(i + ", " + HSV.rgb2hsv(buffer.getLED(i).red, buffer.getLED(i).green, buffer.getLED(i).blue)[0]);
                 out.write(LEDUtilities.colorToString(buffer.getLED(i)));
                 out.write("\n");
             }
